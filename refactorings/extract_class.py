a3a3a769bd879cdac80112328344b534d32a731a
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2020-12-01 00:03:36 +0330

    Implemented FindUsagesListener: Type1, Remained: Type 2

--- refactorings/extract_class.py
+++ refactorings/extract_class.py
@@ -46,8 +46,8 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
 
         field_id = ctx.variableDeclaratorList().variableDeclarator(i=0).variableDeclaratorId().identifier().getText()
         self.field_dict[field_id] = []
-        print("field_id is", field_id)
-        print("_________________")
+        # print("field_id is", field_id)
+        # print("_________________")
 
     # Enter a parse tree produced by Java9_v2Parser#methodDeclaration.
     def enterMethodDeclaration(self, ctx: Java9_v2Parser.MethodDeclarationContext):
@@ -59,16 +59,16 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
         m.append(m_name)
         m.append(self.method_no)
         self.method_name.append(m)
-        print("Method name is:", m_name, " no. = ", self.method_no)
-        print("methods are: ", self.method_name)
-        print(" MMMMMMMMMMMMMMMMMMMMM ")
+        # print("Method name is:", m_name, " no. = ", self.method_no)
+        # print("methods are: ", self.method_name)
+        # print(" MMMMMMMMMMMMMMMMMMMMM ")
 
     # Exit a parse tree produced by Java9_v2Parser#methodDeclaration.
     def exitMethodDeclaration(self, ctx: Java9_v2Parser.MethodDeclarationContext):
         if not self.enter_class:
             return
-        print("fields are", self.field_dict)
-        print(ctx.methodBody().getText())
+        # print("fields are", self.field_dict)
+        # print(ctx.methodBody().getText())
 
     # Exit a parse tree produced by Java9_v2Parser#identifier.
     def exitIdentifier(self, ctx: Java9_v2Parser.IdentifierContext):
@@ -76,16 +76,58 @@ class ExtractClassRefactoringListener(Java9_v2Listener):
             return
         if self.method_no == 0:
             return
-        print("--------------identifier in method ---------")
+        # print("--------------identifier in method ---------")
         current_method = self.method_name[-1]
         variable_name = ctx.getText()
-        print("variable_name =", variable_name)
-        print("current methid name =", current_method)
-        print("field dictionary =", self.field_dict)
+        # print("variable_name =", variable_name)
+        # print("current methid name =", current_method)
+        # print("field dictionary =", self.field_dict)
         if variable_name not in self.field_dict:
-            print(" variable not in dictionary")
+            # print(" variable not in dictionary")
             return
         if not current_method in self.field_dict[variable_name]:
             self.field_dict[variable_name].append(current_method)
-        print("field dictionary =", self.field_dict)
+        # print("field dictionary =", self.field_dict)
 
+
+class FindUsagesListener(Java9_v2Listener):
+    def __init__(self, common_token_stream: CommonTokenStream = None, initial_class=None):
+        if initial_class is None:
+            raise ValueError("Please provide initial class")
+        if common_token_stream is not None:
+            self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
+        else:
+            raise TypeError('common_token_stream is None')
+        self.initial_class = initial_class
+        self.detected_method = None
+        self.method_class_map = {
+            'm2': 'C1'
+        }
+        print("Finding instances for ", self.initial_class)
+
+    def exitClassInstanceCreationExpression_lfno_primary1(self,
+                                                          ctx: Java9_v2Parser.ClassInstanceCreationExpression_lfno_primary1Context):
+        if self.detected_method is not None:
+            print("Class instance creation detected:")
+            class_name = ctx.identifier(0)
+            print(class_name.getText())
+            replace = self.method_class_map.get(self.detected_method)
+            if replace:
+                print("Replace A with", replace)
+                print(
+                    class_name.start.tokenIndex,
+                    class_name.stop.tokenIndex
+                )
+                self.token_stream_rewriter.replaceRange(
+                    from_idx=class_name.start.tokenIndex,
+                    to_idx=class_name.stop.tokenIndex,
+                    text=replace
+                )
+                # print(ctx.identifier(0).start, ctx.identifier(0).end)
+
+    def enterMethodInvocation4(self, ctx: Java9_v2Parser.MethodInvocation4Context):
+        method = ctx.identifier().getText()
+        if method in self.method_class_map.keys():
+            print("Method call detected:")
+            print(method)
+            self.detected_method = method
--- refactorings/input.refactored.java
+++ refactorings/input.refactored.java
@@ -2,11 +2,35 @@
 class A
 {
     public int f; /* public field */
+    public int g;
+    public float k;
+    public float t;
 
     void m(int i)
     {
+        // works on f,k
         this.f = i * this.f;
+        this.k = 67 + this.f;
     }
 
+    int m2(float j)
+    {
+        // works on t,g
+        this.g = j*2 ;
+        this.t = this.g * 3;
+    }
+}
 
+class B
+{
+    public B()
+    {
+        A instance = new A();
+        instance.m2(2.3);
+    }
+
+    void j()
+    {
+        new C1().m2(3.5);
+    }
 }
