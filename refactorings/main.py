73aadfe28cf8d12bb22bf053234c0f34b33bb6ae
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2020-11-30 18:04:08 +0330

    Some empty modules added for example. extract_class.py can detect fields and methods. Next step is separating.

--- refactorings/main.py
+++ refactorings/main.py
@@ -3,6 +3,7 @@ import argparse
 from antlr4 import *
 
 from refactorings.encapsulate_field import EncapsulateFiledRefactoringListener
+from refactorings.extract_class import ExtractClassRefactoringListener
 from refactorings.gen.Java9_v2Lexer import Java9_v2Lexer
 from refactorings.gen.Java9_v2Parser import Java9_v2Parser
 
@@ -11,8 +12,6 @@ def main(args):
     # Step 1: Load input source into stream
     stream = FileStream(args.file, encoding='utf8')
     # input_stream = StdinStream()
-    print('Input stream:')
-    print(stream)
 
     # Step 2: Create an instance of AssignmentStLexer
     lexer = Java9_v2Lexer(stream)
@@ -24,14 +23,11 @@ def main(args):
     # Step 5: Create parse tree
     parse_tree = parser.compilationUnit()
     # Step 6: Create an instance of AssignmentStListener
-    my_listener = EncapsulateFiledRefactoringListener(common_token_stream=token_stream, field_identifier='f')
+    my_listener = ExtractClassRefactoringListener(common_token_stream=token_stream, class_identifier='A')
     walker = ParseTreeWalker()
     walker.walk(t=parse_tree, listener=my_listener)
 
-    print('Compiler result:')
-    print(my_listener.token_stream_rewriter.getDefaultText())
-
-    with open('./gen/input.refactored.java', mode='w', newline='') as f:
+    with open('input.refactored.java', mode='w', newline='') as f:
         f.write(my_listener.token_stream_rewriter.getDefaultText())
 
 
@@ -39,6 +35,6 @@ if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
         '-n', '--file',
-        help='Input source', default=r'./gen/input.java')
+        help='Input source', default=r'input.java')
     args = argparser.parse_args()
     main(args)
