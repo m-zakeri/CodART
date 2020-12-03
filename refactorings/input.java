c99c97acddb574c3e979fe0e6e34d790d425c61e
m-zakeri <m-zakeri@live.com>
2020-12-03 09:46:24 +0330

    Find extract class opportunities

--- refactorings/input.java
+++ refactorings/input.java
@@ -1,12 +1,29 @@
 /* Before refactoring (Original version) */
 class A
 {
-    public int f; /* public field */
+    public int f; /* printF , printF, */
+    public int g; /* printF, printG */
+    public string h; /* printH */
 
-    void m(int i)
+    // Method 1
+    void printF(int i)
     {
         this.f = i * this.f;
     }
 
+    // Method 2
+    void printF(float i){
+        this.f = (int) (i * this.f);
+        this.g = (int) (i * this.g);
+    }
 
+    // Method 3
+    void printG(){
+        print(this.g);
+    }
+
+    // Method 4
+    void printH(){
+        print(this.h);
+    }
 }
