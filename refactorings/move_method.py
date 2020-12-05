a112064504b632a69201b28dab58216bf14768bc
Mina Tahaei <bluxixi@outlook.com>
2020-12-05 15:39:59 +0330

    initialize move_method

--- refactorings/move_method.py
+++ refactorings/move_method.py
@@ -4,7 +4,7 @@ from antlr4.TokenStreamRewriter import TokenStreamRewriter
 from refactorings.gen.Java9_v2Parser import Java9_v2Parser
 from refactorings.gen.Java9_v2Listener import Java9_v2Listener
 
-
+#start move_method
 class MoveMethodRefactoringListener(Java9_v2Listener):
     pass
 
