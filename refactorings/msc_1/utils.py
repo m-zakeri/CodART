56dc8da2f398dd5e93b0c4566a4567bbd2bc6c04
mina <m_khoshbazm@yahoo.com>
2020-12-12 12:49:09 +0330

    complete get_program

--- refactorings/msc_1/utils.py
+++ refactorings/msc_1/utils.py
@@ -7,7 +7,7 @@ from refactorings.msc_1.utils_listener import UtilsListener, Program
 
 def get_program(source_files: list):
     program = Program()
-    for file in list:
+    for file in source_files:
      stream = FileStream(file, encoding='utf8')
      lexer = Java9Lexer(stream)
      token_stream = CommonTokenStream(lexer)
@@ -16,14 +16,13 @@ def get_program(source_files: list):
      listener = UtilsListener()
      walker = ParseTreeWalker()
      walker.walk(listener, tree)
-     for class_name in listener.package.classes:
-      if not str(listener.package.name) in program.packages:
-       program.packages[listener.package.name].classes[class_name] = listener.package.classes[class_name]
-      else:
-       program.packages[listener.package.name].classes[class_name] = listener.package.classes[class_name].append(listener.package.classes[class_name])
-       for p in program.packages:
-        _package = program.packages[p]
-        print(_package)
-        for class_name in _package.classes:
-         print(_package.classes[class_name])
-     return program
+
+     if not str(listener.package.name) in program.packages:
+      program.packages[str(listener.package.name)]=[listener.package.classes]
+     else:
+      program.packages[listener.package.name].append(listener.package.classes)
+    for p in program.packages:
+     _package = program.packages[p]
+     print(_package)
+
+    return program
