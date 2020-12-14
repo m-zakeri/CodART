753d7565edad3c44e620de346ea526c926a98a71
mina <73752456+minakhoshbazm@users.noreply.github.com>
2020-12-14 12:35:37 +0330

    Update utils.py

--- refactorings/msc_1/utils.py
+++ refactorings/msc_1/utils.py
@@ -1,34 +1,40 @@
 from antlr4 import *
+from antlr4_java9.Java9Lexer import Java9Lexer
+from utils_listener import UtilsListener, Java9Parser
+from utils import get_program
 
-from refactorings.msc_1.antlr4_java9.Java9Lexer import Java9Lexer
-from refactorings.msc_1.antlr4_java9.Java9Parser import Java9Parser
-from refactorings.msc_1.utils_listener import UtilsListener, Program
 
+def test_utils_listener():
+    # Test
+    #stream = FileStream("tests/utils_test.java", encoding='utf8')
+    stream = FileStream("antlr4_java9/Test.java", encoding='utf8')
+    lexer = Java9Lexer(stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = Java9Parser(token_stream)
+    tree = parser.compilationUnit()
+    listener = UtilsListener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    print(listener.package)
+    for class_name in listener.package.classes:
+        _class = listener.package.classes[class_name]
+        print(_class)
+        for method_name in _class.methods:
+            print(_class.methods[method_name])
 
-def get_program(source_files: list):
-    program = Program()
-    for file in source_files:
-     size = len(source_files)
-     stream = FileStream(file, encoding='utf8')
-     lexer = Java9Lexer(stream)
-     token_stream = CommonTokenStream(lexer)
-     parser = Java9Parser(token_stream)
-     tree = parser.compilationUnit()
-     listener = UtilsListener()
-     listener.package.name=None
-     listener.package.classes={}
-     walker = ParseTreeWalker()
-     walker.walk(listener, tree)
-     if not str(listener.package.name) in program.packages:
-      program.packages[str(listener.package.name)]=[]
-      for classes_name in listener.package.classes:
-       program.packages[str(listener.package.name)].append(classes_name)
-     else:
-      for classes_name in listener.package.classes:
-       program.packages[str(listener.package.name)].append(classes_name)
+def test_utils():
+    mylist = ["tests/utils_test.java"]
+    program = get_program(mylist)
+    print(program)
+    for package_name in program.packages:
+        package = program.packages[package_name]
+        print(package)
+        for class_name in package.classes:
+            _class = package.classes[class_name]
+            print(_class)
+            for method_name in _class.methods:
+                print(_class.methods[method_name])
 
-    for p in program.packages:
-     _package = program.packages[p]
-     print(_package)
-
-    return program
+if __name__ == "__main__":
+    #test_utils_listener()
+    test_utils()
