7e7e3e12630df570e933f8d996a6ee9cd7c0722e
Majidzadeh <hashpragmaonce@gmail.com>
2020-12-19 14:49:13 +0330

    Write a TODO to add Interface later

--- refactorings/msc_1/utils_listener.py
+++ refactorings/msc_1/utils_listener.py
@@ -63,6 +63,8 @@ class Class(SingleFileElement):
             + " " + str(self.fields) \
             + " " + str(self.methods)
 
+# TODO Add Interface
+
 class Field(SingleFileElement):
     def __init__(self,
                  datatype: str = None,
