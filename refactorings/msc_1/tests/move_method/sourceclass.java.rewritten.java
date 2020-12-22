dc9a19000129552a698a8025e94019b40b7618ed
mina <m_khoshbazm@yahoo.com>
2020-12-22 15:53:00 +0330

    implement move method

---
+++ refactorings/msc_1/tests/move_method/sourceclass.java.rewritten.java
@@ -0,0 +1,10 @@
+
+package tests.utils_test2;
+public class sourceclass
+{
+    private int a;
+    private float b;
+
+  
+
+ }
---
+++ refactorings/msc_1/tests/move_method/targetclass.java.rewritten.java
@@ -0,0 +1,12 @@
+
+package tests.utils_test2;
+public class targetclass
+{
+
+void d(){
+
+  int g=0;
+  }
+
+ }
+}
