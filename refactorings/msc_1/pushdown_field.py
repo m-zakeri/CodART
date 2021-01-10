2fc99e167d4beee4e8089befe06739f5a1a37d1d
Majidzadeh <hashpragmaonce@gmail.com>
2021-01-10 13:51:24 +0330

    Update pushdown_field to use the fast grammar

--- refactorings/msc_1/pushdown_field.py
+++ refactorings/msc_1/pushdown_field.py
@@ -1,6 +1,6 @@
 import os
 
-import utils_listener
+import utils_listener_fast
 import utils
 
 def pushdown_field(source_filenames: list,
@@ -17,12 +17,12 @@ def pushdown_field(source_filenames: list,
             or field_name not in program.packages[package_name].classes[superclass_name].fields:
         return False
 
-    superclass: utils_listener.Class = program.packages[package_name].classes[superclass_name]
+    superclass: utils_listener_fast.Class = program.packages[package_name].classes[superclass_name]
 
     for mn in superclass.methods:
-        m: utils_listener.Method = superclass.methods[mn]
+        m: utils_listener_fast.Method = superclass.methods[mn]
         for item in m.body_local_vars_and_expr_names:
-            if isinstance(item, utils_listener.ExpressionName):
+            if isinstance(item, utils_listener_fast.ExpressionName):
                 if ((len(item.dot_separated_identifiers) == 1
                             and item.dot_separated_identifiers[0] == field_name)
                         or (len(item.dot_separated_identifiers) == 2
@@ -34,9 +34,9 @@ def pushdown_field(source_filenames: list,
     other_derived_classes = []
     classes_to_add_to = []
     for pn in program.packages:
-        p: utils_listener.Package = program.packages[pn]
+        p: utils_listener_fast.Package = program.packages[pn]
         for cn in p.classes:
-            c: utils_listener.Class = p.classes[cn]
+            c: utils_listener_fast.Class = p.classes[cn]
             if ((c.superclass_name == superclass_name and c.file_info.has_imported_class(package_name, superclass_name)) \
                     or (package_name is not None and c.superclass_name == package_name + '.' + superclass_name)):
                 #all_derived_classes.append(c)
@@ -49,13 +49,13 @@ def pushdown_field(source_filenames: list,
                     other_derived_classes.append(c)
     # Check if the field is used from the superclass or other derived classes
     for pn in program.packages:
-        p: utils_listener.Package = program.packages[pn]
+        p: utils_listener_fast.Package = program.packages[pn]
         for cn in p.classes:
-            c: utils_listener.Class = p.classes[cn]
+            c: utils_listener_fast.Class = p.classes[cn]
             has_imported_superclass = c.file_info.has_imported_class(package_name, superclass_name)
             fields_of_superclass_type_or_others = []
             for fn in c.fields:
-                f: utils_listener.Field = c.fields[fn]
+                f: utils_listener_fast.Field = c.fields[fn]
                 if (f.datatype == superclass_name and has_imported_superclass) \
                         or (package_name is not None and f.datatype == (package_name + '.' + superclass_name)):
                     fields_of_superclass_type_or_others.append(f.name)
@@ -63,17 +63,17 @@ def pushdown_field(source_filenames: list,
                         or f.datatype == (o.package_name + '.' + o.name) for o in other_derived_classes):
                     fields_of_superclass_type_or_others.append(f.name)
             for mn in c.methods:
-                m: utils_listener.Method = c.methods[mn]
+                m: utils_listener_fast.Method = c.methods[mn]
                 local_vars_of_superclass_type_or_others = []
                 for item in m.body_local_vars_and_expr_names:
-                    if isinstance(item, utils_listener.LocalVariable):
+                    if isinstance(item, utils_listener_fast.LocalVariable):
                         if (item.datatype == superclass_name and has_imported_superclass) \
                                 or item.datatype == (package_name + '.' + superclass_name):
                             local_vars_of_superclass_type_or_others.append(item.identifier)
                         if any((c.file_info.has_imported_class(o.package_name, o.name) and item.datatype == o.name)
                                 or item.datatype == (o.package_name + '.' + o.name) for o in other_derived_classes):
                             local_vars_of_superclass_type_or_others.append(item.identifier)
-                    elif isinstance(item, utils_listener.ExpressionName):
+                    elif isinstance(item, utils_listener_fast.ExpressionName):
                         if item.dot_separated_identifiers[-1] == field_name \
                                 and (
                                     (len(item.dot_separated_identifiers) == 2)
@@ -93,19 +93,19 @@ def pushdown_field(source_filenames: list,
         i = field.index_in_variable_declarators
         var_ctxs = field.all_variable_declarator_contexts
         if i == 0:
-            to_remove = utils_listener.TokensInfo(var_ctxs[i])
-            to_remove.stop = utils_listener.TokensInfo(var_ctxs[i + 1]).start - 1  # Include the ',' after it
+            to_remove = utils_listener_fast.TokensInfo(var_ctxs[i])
+            to_remove.stop = utils_listener_fast.TokensInfo(var_ctxs[i + 1]).start - 1  # Include the ',' after it
             rewriter.replace(to_remove, "")
         else:
-            to_remove = utils_listener.TokensInfo(var_ctxs[i])
-            to_remove.start = utils_listener.TokensInfo(var_ctxs[i - 1]).stop + 1  # Include the ',' before it
+            to_remove = utils_listener_fast.TokensInfo(var_ctxs[i])
+            to_remove.start = utils_listener_fast.TokensInfo(var_ctxs[i - 1]).stop + 1  # Include the ',' before it
             rewriter.replace(to_remove, "")
 
     is_public = "public" in field.modifiers
     is_protected = "protected" in field.modifiers
     modifier = ("public " if is_public else ("protected " if is_protected else ""))
     for c in classes_to_add_to:
-        c_body_start = utils_listener.TokensInfo(c.parser_context.classBody())
+        c_body_start = utils_listener_fast.TokensInfo(c.parser_context.classBody())
         c_body_start.stop = c_body_start.start  # Start and stop both point to the '{'
         rewriter.insert_after(c_body_start, "\n    " + modifier + field.datatype + " " + field_name \
                               + ((" = " + field.initializer) if field.initializer is not None else "")
