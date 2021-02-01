097d2273fd09320fd7b689faa58daf580dfd78ba
Milad Esfandiyari <miladisjobs@gmail.com>
2021-02-01 16:09:05 +0330

    update .gitignore to ignore refactored folder

--- input.refactored.java
+++ input.refactored.java
@@ -1,30 +1,77 @@
-/* Before refactoring (Original version) */
+package org.json;
 
-class A
-{
-    public int f; /* printF , printF, */
-    public int g; /* printF, printG */
+/*
+Copyright (c) 2002 JSON.org
 
-    // Method 1
-    void printF(int i)
-    {
-        this.f = i * this.f;
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+The Software shall be used for Good, not Evil.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
+ * The HTTPTokener extends the JSONTokener to provide additional methods
+ * for the parsing of HTTP headers.
+ * @author JSON.org
+ * @version 2015-12-09
+ */
+public class HTTPTokener extends JSONTokener {
+
+    /**
+     * Construct an HTTPTokener from a string.
+     * @param string A source string.
+     */
+    public HTTPTokener(String string) {
+        super(string);
     }
 
-    // Method 2
-    void printF(float i){
-        this.f = (int) (i * this.f);
-        this.g = (int) (i * this.g);
-    }
 
-    // Method 3
-    void printG(){
-        print(this.g);
-    }
-
-    public string h;
-
-    void printH(){
-        print(this.h);
+    /**
+     * Get the next token or string. This is used in parsing HTTP headers.
+     * @return A String.
+     * @throws JSONException if a syntax error occurs
+     */
+    public String nextToken() throws JSONException {
+        char c;
+        char q;
+        StringBuilder sb = new StringBuilder();
+        do {
+            c = next();
+        } while (Character.isWhitespace(c));
+        if (c == '"' || c == '\'') {
+            q = c;
+            for (;;) {
+                c = next();
+                if (c < ' ') {
+                    throw syntaxError("Unterminated string.");
+                }
+                if (c == q) {
+                    return sb.toString();
+                }
+                sb.append(c);
+            }
+        }
+        for (;;) {
+            if (c == 0 || Character.isWhitespace(c)) {
+                return sb.toString();
+            }
+            sb.append(c);
+            c = next();
+        }
     }
 }
