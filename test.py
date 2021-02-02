7d46933c5d5faf8885224d541df8da6b3a68b10e
DESKTOP-HKSAG3B\LENOVO <reezaaghaahremani@yahoo.com>
2021-02-02 15:05:22 +0330

    changed code in test.py

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ __author__ = 'Morteza'
 
 
 import argparse
+import os
 
 from antlr4 import *
 
@@ -22,21 +23,26 @@ from gen.java.JavaParser import JavaParser
 
 
 def main(args):
-    # Step 1: Load input source into stream
-    stream = FileStream(args.file, encoding='utf8')
-    # input_stream = StdinStream()
+    input1_className = input()
+    input2_className = input()
+    x = args.directory
+    z = [file for file in os.listdir(x) if '.java' in file]
+    for file in z:
+        # Step 1: Load input source into stream
+        stream = FileStream(x + '/' + file, encoding='utf8')
+        # input_stream = StdinStream()
 
-    # Step 2: Create an instance of AssignmentStLexer
-    lexer = JavaLexer(stream)
-    # Step 3: Convert the input source into a list of tokens
-    token_stream = CommonTokenStream(lexer)
-    # Step 4: Create an instance of the AssignmentStParser
-    parser = JavaParser(token_stream)
-    tree = parser.compilationUnit()
+        # Step 2: Create an instance of AssignmentStLexer
+        lexer = JavaLexer(stream)
+        # Step 3: Convert the input source into a list of tokens
+        token_stream = CommonTokenStream(lexer)
+        # Step 4: Create an instance of the AssignmentStParser
+        parser = JavaParser(token_stream)
+        tree = parser.compilationUnit()
 
-    # Step 6: Create an instance of AssignmentStListener
+
     my_listener = InlineClassRefactoringListener(
-        common_token_stream=token_stream, source_class='A_New', target_class='A'
+        common_token_stream=token_stream, source_class=input1_className, target_class=input2_className
     )
 
     walker = ParseTreeWalker()
@@ -49,7 +55,7 @@ def main(args):
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        '-n', '--file',
-        help='Input source', default=r'./input.java')
+        '-d', '--directory',
+        help='Input source', default=r'testproject/input/src/main/java/org/json')
     args = argparser.parse_args()
     main(args)
