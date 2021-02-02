dd1a5242e1e3f24c1ef4d19ea691061ed4f7adba
Milad Esfandiyari <miladisjobs@gmail.com>
2021-02-02 09:26:12 +0330

    change java lexer and parser to new version

--- test.py
+++ test.py
@@ -17,40 +17,48 @@ import os
 from antlr4 import *
 
 from refactorings.collapse_hierarchy import CollapseHierarchyRefactoringListener
-from gen.java.JavaLexer import JavaLexer
-from gen.java.JavaParser import JavaParser
+from gen.javaLabeled.JavaLexer import JavaLexer
+from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
 
 
 
 def main(args):
     x = args.directory
     z = [file for file in os.listdir(x) if '.java' in file]
-    for file in z:
-        # Step 1: Load input source into stream
-        stream = FileStream(x+'/'+file, encoding='utf8')
-        # input_stream = StdinStream()
+    source_class_data = None
+    target_class = None
+    target_class_data = None
+    for i in range(3):
+        for file in z:
 
-        # Step 2: Create an instance of AssignmentStLexer
-        lexer = JavaLexer(stream)
-        # Step 3: Convert the input source into a list of tokens
-        token_stream = CommonTokenStream(lexer)
-        # Step 4: Create an instance of the AssignmentStParser
-        parser = JavaParser(token_stream)
-        tree = parser.compilationUnit()
+            # Step 1: Load input source into stream
+            stream = FileStream(x+'/'+file, encoding='utf8')
+            # input_stream = StdinStream()
 
-        # Step 6: Create an instance of AssignmentStListener
-        my_listener = CollapseHierarchyRefactoringListener(
-            common_token_stream=token_stream, source_class='JSONPointerException'
-        )
+            # Step 2: Create an instance of AssignmentStLexer
+            lexer = JavaLexer(stream)
+            # Step 3: Convert the input source into a list of tokens
+            token_stream = CommonTokenStream(lexer)
+            # Step 4: Create an instance of the AssignmentStParser
+            parser = JavaParserLabeled(token_stream)
+            tree = parser.compilationUnit()
+            if file == 'JSONPointer.java':
+                a = 10
+            # Step 6: Create an instance of AssignmentStListener
+            my_listener = CollapseHierarchyRefactoringListener(
+                common_token_stream=token_stream, source_class='JSONPointerException',
+                target_class=target_class, source_class_data=source_class_data,
+                target_class_data=target_class_data
+            )
 
-        walker = ParseTreeWalker()
-        walker.walk(t=tree, listener=my_listener)
-        if my_listener.is_source_class:
-            z = file
-            print(z)
+            walker = ParseTreeWalker()
+            walker.walk(t=tree, listener=my_listener)
+            target_class = my_listener.target_class
+            source_class_data = my_listener.source_class_data
+            target_class_data = my_listener.target_class_data
 
-        with open('testproject/refactored/'+file, mode='w+', newline='') as f:
-            f.write(my_listener.token_stream_rewriter.getDefaultText())
+            with open('testproject/refactored/'+file, mode='w+', newline='') as f:
+                f.write(my_listener.token_stream_rewriter.getDefaultText())
 
 
 if __name__ == '__main__':
