f89e863767cfb3cb4d8bcd341f519fcc83019646
m-zakeri <m-zakeri@live.com>
2021-03-19 00:24:39 +0330

    Add configuration for windows

--- utilization/setup_understand.py
+++ utilization/setup_understand.py
@@ -1,6 +1,23 @@
+"""
+This is configuration file for understand path
+You should set the path of understand installation in this script
+
+"""
 import os
+import sys
 
-LD_LIBRARY_PATH = "/home/ali/scitools/bin/linux64/"
-PYTHONPATH = "/home/ali/scitools/bin/linux64/Python"
 
-os.environ["LD_LIBRARY_PATH"] = LD_LIBRARY_PATH
+# -------------------
+# For Linux os
+LD_LIBRARY_PATH = "/home/ali/scitools/bin/linux64/"  # Put your path here
+PYTHONPATH = "/home/ali/scitools/bin/linux64/Python"  # Put your path here
+
+os.environ["LD_LIBRARY_PATH"] = LD_LIBRARY_PATH  # Put your path here
+
+
+# -------------------
+# For Windows os
+# https://scitools.com/support/python-api/
+# Python 3.8 and newer require the user add a call to os.add_dll_directory(“SciTools/bin/“  # Put your path here
+# os.add_dll_directory('C:/Program Files/SciTools/bin/pc-win64')  # Put your path here
+sys.path.insert(0, 'D:/program files/scitools/bin/pc-win64/python')  # Put your path here
