88eef0c97de9bf0cc17c53bfab8c0d7038b24bb6
m-zakeri <m-zakeri@live.com>
2021-03-20 00:28:58 +0330

    Add support switching between different Java grammar

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 """
 
 The main module of CodART
+This module implements CodART CLI
+To find and run more unit and integration tests please look at `benchmark_projects` and `test` directory
 
 -changelog
--- Add C++ backend support
+-- Add support switching between different Java grammar
+-- Add JavaParserLabeled.g4
+-- Add C++ backend support for Java9_v2.g2
+-- Start with encapsulate field refactoring
 
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Morteza'
-
 
 import argparse
 
 from antlr4 import *
 
-from refactorings.extract_class import ExtractClassRefactoringListener
-from gen.java9 import Java9_v2Lexer
-from gen.java9.Java9_v2Parser import Java9_v2Parser
+# Import different grammars
+# from gen.java9.Java9_v2Lexer import Java9_v2Lexer  # Old slow grammar lexer
+# from gen.java9.Java9_v2Parser import Java9_v2Parser  # Old slow grammar parser
+# from java9speedy.parser import sa_java9_v2  # Old slow grammar enhanced by CPP backend
 
-from java9speedy.parser import sa_java9_v2
+from gen.javaLabeled.JavaLexer import JavaLexer  # Java8 grammar efficient lexer
+from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled  # Java8 grammar efficient parser labeled
+
+# Import refactorings listeners
+from refactorings.encapsulate_field import EncapsulateFiledRefactoringListener  # CodART first refactoring :)
+from refactorings.extract_class import ExtractClassRefactoringListener
 
 
 def main(args):
@@ -28,36 +38,41 @@ def main(args):
     # input_stream = StdinStream()
 
     # Step 2: Create an instance of AssignmentStLexer
-    lexer = Java9_v2Lexer(stream)
+    # lexer = Java9_v2Lexer(stream)  # Deprecated
+    lexer = JavaLexer(stream) # Current lexer
+
     # Step 3: Convert the input source into a list of tokens
-    token_stream = CommonTokenStream(lexer)
+    common_token_stream = CommonTokenStream(lexer)
+
     # Step 4: Create an instance of the AssignmentStParser
-    parser = Java9_v2Parser(token_stream)
-    parser.getTokenStream()
+    # parser = Java9_v2Parser(common_token_stream)  # Deprecated
+    parser = JavaParserLabeled(common_token_stream)  # Current parser
+    # parser.getTokenStream()
 
     # Step 5: Create parse tree
-    # 1. Python backend --> Low speed
-    # parse_tree = parser.compilationUnit()
+    # 5.1. Python backend --> Low speed
+    parse_tree = parser.compilationUnit()
 
-    # 2. C++ backend --> high speed
+    # 5.2. C++ backend --> high speed
+    # parse_tree = sa_java9_v2.parse(stream, 'compilationUnit', None) # Deprecated
 
-    parse_tree = sa_java9_v2.parse(stream, 'compilationUnit', None)
-    quit()
     # Step 6: Create an instance of AssignmentStListener
-    my_listener = ExtractClassRefactoringListener(common_token_stream=token_stream, class_identifier='Worker')
+    my_listener = EncapsulateFiledRefactoringListener(common_token_stream=common_token_stream, field_identifier='f')
+    # my_listener = ExtractClassRefactoringListener(common_token_stream=token_stream, class_identifier='Worker')
 
     # return
     walker = ParseTreeWalker()
     walker.walk(t=parse_tree, listener=my_listener)
 
-    with open('input.refactored.java', mode='w', newline='') as f:
+    with open('tests/encapsulate_field_tests/input.refactored.java', mode='w', newline='') as f:
         f.write(my_listener.token_stream_rewriter.getDefaultText())
 
 
 if __name__ == '__main__':
+    '''Simple CodART CLI'''
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
         '-n', '--file',
-        help='Input source', default=r'../grammars/Test.java')
+        help='Input source', default=r'tests/encapsulate_field_tests/input.java')  # Please use only relative and relevant (e.g. point to tests) path
     args = argparser.parse_args()
     main(args)
