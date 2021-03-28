3d710460553c55b2cb7058eacce160d2d38d2934
Seyyed Ali Ayati <aliayati752@outlook.com>
2021-03-28 16:51:23 +0430

    moved make_field_static.py to main refactoring module

--- refactorings/make_field_static.py
+++ refactorings/make_field_static.py
@@ -1,21 +1,15 @@
-"""
-The scripts implements different refactoring operations
+from gen.javaLabeled.JavaLexer import JavaLexer
 
-
-"""
-__version__ = '0.1.0'
-__author__ = 'Morteza'
-
-import networkx as nx
+try:
+    import understand as und
+except ImportError as e:
+    print(e)
 
 from antlr4 import *
 from antlr4.TokenStreamRewriter import TokenStreamRewriter
 
-from gen.java9.Java9_v2Parser import Java9_v2Parser
-from gen.java9 import Java9_v2Listener
 from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
 from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
-import visualization.graph_visualization
 
 
 class MakeFieldStaticRefactoringListener(JavaParserLabeledListener):
@@ -24,7 +18,7 @@ class MakeFieldStaticRefactoringListener(JavaParserLabeledListener):
     Creates a new class and move fields and methods from the old class to the new one
     """
 
-    def __init__(self, common_token_stream: CommonTokenStream = None, source_class=None, field_name:str = None):
+    def __init__(self, common_token_stream: CommonTokenStream = None, source_class=None, field_name: str = None):
 
         if field_name is None:
             self.field_name = ""
@@ -41,9 +35,9 @@ class MakeFieldStaticRefactoringListener(JavaParserLabeledListener):
             self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
 
         self.is_source_class = False
-        self.is_static=False
+        self.is_static = False
 
-    def enterClassDeclaration(self, ctx:JavaParserLabeled.ClassDeclarationContext):
+    def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         print("Refactoring started, please wait...")
         class_identifier = ctx.IDENTIFIER().getText()
         if class_identifier == self.source_class:
@@ -51,29 +45,55 @@ class MakeFieldStaticRefactoringListener(JavaParserLabeledListener):
         else:
             self.is_source_class = False
 
-    def exitFieldDeclaration(self, ctx:JavaParserLabeled.FieldDeclarationContext):
+    def exitFieldDeclaration(self, ctx: JavaParserLabeled.FieldDeclarationContext):
         if not self.is_source_class:
             return None
         grand_parent_ctx = ctx.parentCtx.parentCtx
         # field_identifier = ctx.variableDeclarators().getText().split(",")
         field_identifier = ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().IDENTIFIER().getText()
         if self.field_name in field_identifier:
-            if grand_parent_ctx.modifier()==[]:
+            if grand_parent_ctx.modifier() == []:
                 self.token_stream_rewriter.replaceRange(
                     from_idx=ctx.typeType().start.tokenIndex,
                     to_idx=ctx.typeType().stop.tokenIndex,
-                    text='static '+ ctx.typeType().getText()
+                    text='static ' + ctx.typeType().getText()
                 )
             else:
-                for i in range(0,len(grand_parent_ctx.modifier())):
-                    if grand_parent_ctx.modifier(i).getText()=="static":
-                        self.is_static=True
+                for i in range(0, len(grand_parent_ctx.modifier())):
+                    if grand_parent_ctx.modifier(i).getText() == "static":
+                        self.is_static = True
                         break
                 if not self.is_static:
                     self.token_stream_rewriter.replaceRange(
                         from_idx=grand_parent_ctx.modifier(0).start.tokenIndex,
                         to_idx=grand_parent_ctx.modifier(0).stop.tokenIndex,
-                        text=grand_parent_ctx.modifier(0).getText()+' static'
+                        text=grand_parent_ctx.modifier(0).getText() + ' static'
                     )
 
         print("Finished Processing...")
+
+
+if __name__ == '__main__':
+    udb_path = "/home/ali/Desktop/code/TestProject/TestProject.udb"
+    source_class = "Triangle"
+    field_name = "TEST"
+    # initialize with understand
+    main_file = ""
+    db = und.open(udb_path)
+    for cls in db.ents("class"):
+        if cls.simplename() == source_class:
+            main_file = cls.parent().longname()
+
+    stream = FileStream(main_file, encoding='utf8')
+    lexer = JavaLexer(stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = JavaParserLabeled(token_stream)
+    parser.getTokenStream()
+    parse_tree = parser.compilationUnit()
+    my_listener = MakeFieldStaticRefactoringListener(common_token_stream=token_stream, source_class=source_class,
+                                                     field_name=field_name)
+    walker = ParseTreeWalker()
+    walker.walk(t=parse_tree, listener=my_listener)
+
+    with open(main_file, mode='w', newline='') as f:
+        f.write(my_listener.token_stream_rewriter.getDefaultText())
