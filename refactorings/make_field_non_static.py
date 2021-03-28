69813ec51fa8a0b01be0e83fd972e8c9d7c0bbc6
Seyyed Ali Ayati <aliayati752@outlook.com>
2021-03-28 12:30:36 +0430

    fixed make_field_non_static.py

--- refactorings/make_field_non_static.py
+++ refactorings/make_field_non_static.py
@@ -1,95 +1,93 @@
+from gen.javaLabeled.JavaLexer import JavaLexer
+
+try:
+    import understand as und
+except ImportError as e:
+    print(e)
+
 from antlr4 import *
 from antlr4.TokenStreamRewriter import TokenStreamRewriter
 
-from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
 from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
+from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
 
 
 class MakeFieldNonStaticRefactoringListener(JavaParserLabeledListener):
     """
-    To implement the encapsulate filed refactored
-    Encapsulate field: Make a public field private and provide accessors
+    To implement extract class refactoring based on its actors.
+    Creates a new class and move fields and methods from the old class to the new one
     """
 
-    def __init__(self, common_token_stream: CommonTokenStream = None,
-                 field_identifier: str = None, class_identifier: str = None, package_identifier: str = None):
-        """
-        :param common_token_stream:
-        """
-        self.token_stream = common_token_stream
-        self.field_identifier = field_identifier
-        self.class_identifier = class_identifier
-        self.package_identifier = package_identifier
-        self.declared_objects_names = []
+    def __init__(self, common_token_stream: CommonTokenStream = None, source_class=None, field_name: str = None):
 
-        self.is_package_imported = False
-        self.in_selected_package = False
-        self.in_selected_class = False
-        self.in_some_package = False
-        self.class_start_index = 0
-        self.canceled = False
-
-        self.placeholder_field_identifier = "placeholder"
-
-        # Move all the tokens in the source code in a buffer, token_stream_rewriter.
-        if common_token_stream is not None:
-            self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
+        if field_name is None:
+            self.field_name = ""
         else:
-            raise TypeError('common_token_stream is None')
+            self.field_name = field_name
 
-    def enterPackageDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext):
-        self.in_some_package = True
-        if self.package_identifier is not None:
-            if self.package_identifier == ctx.qualifiedName().getText():
-                self.in_selected_package = True
-                print("Package Found")
+        if source_class is None:
+            self.source_class = ""
+        else:
+            self.source_class = source_class
+        if common_token_stream is None:
+            raise ValueError('common_token_stream is None')
+        else:
+            self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
+
+        self.is_source_class = False
+        self.is_static = False
 
     def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
-
-        if self.package_identifier is None and not self.in_some_package or \
-                self.package_identifier is not None and self.in_selected_package:
-            if ctx.IDENTIFIER().getText() == self.class_identifier:
-                print("Class Found")
-                self.in_selected_class = True
-
-    def enterImportDeclaration(self, ctx: JavaParserLabeled.ImportDeclarationContext):
-        if self.package_identifier is not None:
-            if ctx.getText() == "import" + self.package_identifier + "." + self.class_identifier + ";" \
-                    or ctx.getText() == "import" + self.package_identifier + ".*" + ";" \
-                    or ctx.getText() == "import" + self.package_identifier + ";":
-                self.is_package_imported = True
+        print("Refactoring started, please wait...")
+        class_identifier = ctx.IDENTIFIER().getText()
+        if class_identifier == self.source_class:
+            self.is_source_class = True
+        else:
+            self.is_source_class = False
 
     def exitFieldDeclaration(self, ctx: JavaParserLabeled.FieldDeclarationContext):
-        if self.package_identifier is None and not self.in_some_package \
-                or self.package_identifier is not None and self.in_selected_package:
-            if self.in_selected_class:
-                if ctx.variableDeclarators().variableDeclarator(0) \
-                        .variableDeclaratorId().getText() == self.field_identifier:
+        if not self.is_source_class:
+            return None
+        grand_parent_ctx = ctx.parentCtx.parentCtx
+        # field_identifier = ctx.variableDeclarators().getText().split(",")
+        field_identifier = ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().IDENTIFIER().getText()
+        if self.field_name in field_identifier:
+            if not (grand_parent_ctx.modifier() == []):
+                for i in range(0, len(grand_parent_ctx.modifier())):
+                    if grand_parent_ctx.modifier(i).getText() == "static":
+                        self.is_static = True
+                        break
+                if self.is_static:
+                    self.token_stream_rewriter.replaceRange(
+                        from_idx=grand_parent_ctx.modifier(i).start.tokenIndex,
+                        to_idx=grand_parent_ctx.modifier(i).stop.tokenIndex,
+                        text=''
+                    )
 
-                    grand_parent_ctx = ctx.parentCtx.parentCtx
+        print("Finished Processing...")
 
-                    if len(grand_parent_ctx.modifier()) > 0:
-                        for i in range(len(grand_parent_ctx.modifier())):
-                            if grand_parent_ctx.modifier(i).getText() == "static":
-                                self.token_stream_rewriter.replaceIndex(index=grand_parent_ctx.start.tokenIndex + i,
-                                                                        text=" ")
-                                break
 
-        if self.package_identifier is None or self.package_identifier is not None and self.is_package_imported:
-            if ctx.typeType().classOrInterfaceType() is not None:
-                if ctx.typeType().classOrInterfaceType().getText() == self.class_identifier:
-                    self.declared_objects_names.append(ctx.variableDeclarators().variableDeclarator(0)
-                                                       .variableDeclaratorId().getText())
-                    print("Object " + ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText() \
-                          + " of type " + self.class_identifier + " found.")
+if __name__ == '__main__':
+    udb_path = "/home/ali/Desktop/code/TestProject/TestProject.udb"
+    source_class = "Website"
+    field_name = "HELLO_FROM_STUDENT_WEBSITE"
+    # initialize with understand
+    main_file = ""
+    db = und.open(udb_path)
+    for cls in db.ents("class"):
+        if cls.simplename() == source_class:
+            main_file = cls.parent().longname()
 
-    def enterExpression1(self, ctx: JavaParserLabeled.Expression1Context):
-        if self.is_package_imported or self.package_identifier is None or self.in_selected_package:
-            if ctx.getText() == self.class_identifier + "." + self.field_identifier:
-                if len(self.declared_objects_names) == 0:
-                    print("No object found...canceling ")
-                    self.canceled = True
-                    return
-                self.token_stream_rewriter.replaceIndex(
-                    index=ctx.start.tokenIndex,
-                    text=self.declared_objects_names[0])
+    stream = FileStream(main_file, encoding='utf8')
+    lexer = JavaLexer(stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = JavaParserLabeled(token_stream)
+    parser.getTokenStream()
+    parse_tree = parser.compilationUnit()
+    my_listener = MakeFieldNonStaticRefactoringListener(common_token_stream=token_stream, source_class=source_class,
+                                                        field_name=field_name)
+    walker = ParseTreeWalker()
+    walker.walk(t=parse_tree, listener=my_listener)
+
+    with open(main_file, mode='w', newline='') as f:
+        f.write(my_listener.token_stream_rewriter.getDefaultText())
