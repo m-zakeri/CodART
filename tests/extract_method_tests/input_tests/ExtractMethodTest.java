1197e842e89ee447e6e4ec18f2be155ffca73dbf
sinaziaee <sinaziaee99@gmail.com>
2021-05-19 20:43:27 +0430

    updated extract_method

---
+++ tests/extract_method_tests/input_tests/ExtractMethodTest.java
@@ -0,0 +1,15 @@
+package example;
+public class ExtractMethodTest{
+    public void main(int x, long y) {
+        int z = 0;
+        long k = 1;
+        System.out.println("name: "+ y );
+        int f = 0;
+        System.out.println("name: "+ f );
+        for (int x = 0;x<5;x++)
+        {
+            System.out.println("amount: "+z);
+            System.out.println("name: "+ f );
+        }
+    }
+}
