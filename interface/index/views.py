09b99ca54dc3dbb5abcb21d20a8b1fddfbc174bf
Seyyed Ali Ayati <aliayati752@outlook.com>
2021-05-26 15:35:37 +0430

    Added refactorings module to django interface

--- interface/index/views.py
+++ interface/index/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from django.conf import settings
+
+
+def import_refactorings():
+    import sys
+    import os
+    sys.path.append(os.path.dirname(settings.BASE_DIR))
+    module = __import__('refactorings')  # import code
+    sys.path.pop()
+    return module
+
+
+refactorings = import_refactorings()
+refactoring_map = refactorings.refactoring_map
 
 
 def index(request):
     context = {
         'index': True
     }
+
+    print(refactoring_map)
+
     return render(request, 'index/index.html', context)
 
 
--- refactorings/__init__.py
+++ refactorings/__init__.py
@@ -1,1 +1,6 @@
 # from refactorings import collapse_hierarchy
+from . import make_field_static
+
+refactoring_map = {
+    'make_field_static': make_field_static
+}
