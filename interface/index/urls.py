0c474804b791c24fc169f08dd40127657db54a12
Seyyed Ali Ayati <aliayati752@outlook.com>
2021-05-26 14:31:24 +0430

    Added about us in sidebar.html

--- interface/index/urls.py
+++ interface/index/urls.py
@@ -4,4 +4,5 @@ from index import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about-us/', views.about_us, name='about_us')
 ]
--- interface/index/views.py
+++ interface/index/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index/index.html')
+    context = {
+        'index': True
+    }
+    return render(request, 'index/index.html', context)
+
+
+def about_us(request):
+    context = {
+        'about_us': True
+    }
+    return render(request, 'index/about_us.html', context)
