6795ad52f1914cfe2f2f263a8c88d3ffe15a33dd
Pouorix <pouorix@gmail.com>
2021-06-22 04:21:03 +0430

    Fix:propagation source class usage situation

--- refactorings/move_method.py
+++ refactorings/move_method.py
@@ -30,7 +30,6 @@ class MoveMethodRefactoring:
         if self.method_key not in program.packages[self.package_name].classes[self.class_name].methods:
             return False
 
-
         _sourceclass = program.packages[self.package_name].classes[self.class_name]
         _targetclass = program.packages[self.target_package_name].classes[self.target_class_name]
         _method = program.packages[self.package_name].classes[self.class_name].methods[self.method_key]
@@ -95,7 +94,8 @@ class MoveMethodRefactoring:
                 ii = _method.body_method_invocations_without_typename[i]
                 for j in ii:
                     i_tokens = TokensInfo(j)
-                    token_stream_rewriter.insertBeforeIndex(index=i_tokens.start, text=str.lower(self.class_name) + ".")
+                    token_stream_rewriter.insertBeforeIndex(index=i_tokens.start, text=str.lower(self.class_name) + str(
+                        int(time.time())) + ".")
         # Rewriter_.insert_before_start(target_method_parser,'sdsddsds')
         # pass object of source.java class to method
         # if param_tokens_info.start is not None:
@@ -132,40 +132,66 @@ class MoveMethodRefactoring:
             package_item_dic = package[package_item]
             for classes_item in package_item_dic.classes:
                 class_item_dic = package_item_dic.classes[classes_item]
-                for import_item in class_item_dic.file_info.class_imports:
-                    if (self.class_name == import_item.class_name):
-                        for method_item in class_item_dic.methods:
-                            method_item_dict = class_item_dic.methods[method_item]
-                            tempIndex = method_item_dict.body_text.find('=new' + self.class_name + '()')
-                            tempIndex2 = method_item_dict.body_text[:tempIndex].find(self.class_name)
-                            tempIndex2 = tempIndex2 + len(self.class_name)
-                            instance_name = method_item_dict.body_text[tempIndex2:tempIndex]
-                            my_line = instance_name + '.' + self.method_key
-                            if my_line in method_item_dict.body_text:
-                                import_parser = TokensInfo(import_item.parser_context)
-                                if self.target_package_name != class_item_dic.package_name:
+                if classes_item == self.class_name:  # check any method in source class use that function or not
+                    sourceClass = package[self.package_name].classes[self.class_name]
+                    for method_item in sourceClass.methods:
+                        method_item_dict = sourceClass.methods[method_item]
+                        for i in method_item_dict.body_method_invocations_without_typename:
+                            if i.getText() == self.class_name:
+                                import_parser = TokensInfo(
+                                    sourceClass.parser_context)
+                                if self.target_package_name != self.package_name:  # 1-import package-check to don't import self package
                                     rewriter.insert_after(import_parser,
                                                           '\nimport ' + self.target_package_name + '.' + self.target_class_name + ';')
-                                new_instance_name = instance_name + str(int(time.time()))
-                                method_parser = TokensInfo(
-                                    method_item_dict.body_local_vars_and_expr_names[0].parser_context)
-                                if self.target_class_name != class_item_dic.name:
-                                    rewriter.insert_before_start(method_parser,
-                                                                 self.target_class_name + ' ' + new_instance_name + ' = new ' + self.target_class_name + '();\n')
-                                for body_item in method_item_dict.body_local_vars_and_expr_names:
-                                    try:
-                                        if body_item.dot_separated_identifiers[0] == instance_name and \
-                                                body_item.dot_separated_identifiers[1] == self.method_key.split('(')[0]:
-                                            instance_parser = TokensInfo(body_item.parser_context)
-                                            if self.target_class_name != class_item_dic.name:
-                                                rewriter.replace(instance_parser,
-                                                                 new_instance_name + '.' + self.method_key)
-                                            else:
-                                                rewriter.replace(instance_parser, self.method_key)
-                                    except:
-                                        continue
-                                rewriter.apply()
-                                #self.__reformat(class_item_dic, class_item_dic)
+                                ii = method_item_dict.body_method_invocations_without_typename[i] # create instance before where our target function usage
+                                for j in ii:
+                                    i_tokens = TokensInfo(j)
+                                    rewriter.insert_before_start(i_tokens,
+                                                                 self.target_class_name + ' ' + self.target_class_name.lower() + str(
+                                                                     int(
+                                                                         time.time())) + ' = new ' + self.target_class_name + '();\n' + self.target_class_name.lower() + str(
+                                                                     int(
+                                                                         time.time())) + '.')
+                                    rewriter.apply()
+
+                else:
+                    for import_item in class_item_dic.file_info.class_imports:
+                        if (
+                                self.class_name == import_item.class_name):  # check if the class has the import of our sorce class or not
+                            for method_item in class_item_dic.methods:
+                                method_item_dict = class_item_dic.methods[
+                                    method_item]  # check that class use our target method or not ***
+                                tempIndex = method_item_dict.body_text.find('=new' + self.class_name + '()')
+                                tempIndex2 = method_item_dict.body_text[:tempIndex].find(self.class_name)
+                                tempIndex2 = tempIndex2 + len(self.class_name)
+                                instance_name = method_item_dict.body_text[tempIndex2:tempIndex]
+                                my_line = instance_name + '.' + self.method_key
+                                if my_line in method_item_dict.body_text:  # check that class use our target method or not ***
+                                    import_parser = TokensInfo(import_item.parser_context)
+                                    if self.target_package_name != class_item_dic.package_name:  # check to dont import self package
+                                        rewriter.insert_after(import_parser,
+                                                              '\nimport ' + self.target_package_name + '.' + self.target_class_name + ';')
+                                    new_instance_name = self.target_class_name.lower() + str(
+                                        int(time.time()))  # this trick will create a unique name
+                                    method_parser = TokensInfo(
+                                        method_item_dict.body_local_vars_and_expr_names[0].parser_context)
+                                    if self.target_class_name != class_item_dic.name:  # dont create a instace of self class ( if target class is our selecting class )
+                                        rewriter.insert_before_start(method_parser,
+                                                                     self.target_class_name + ' ' + new_instance_name + ' = new ' + self.target_class_name + '();\n')
+                                    for body_item in method_item_dict.body_local_vars_and_expr_names:
+                                        try:
+                                            if body_item.dot_separated_identifiers[0] == instance_name and \
+                                                    body_item.dot_separated_identifiers[1] == \
+                                                    self.method_key.split('(')[0]:
+                                                instance_parser = TokensInfo(body_item.parser_context)
+                                                if self.target_class_name != class_item_dic.name:  # if target class is our selecting class dont write myClass.function instead of function
+                                                    rewriter.replace(instance_parser,
+                                                                     new_instance_name + '.' + self.method_key)
+                                                else:
+                                                    rewriter.replace(instance_parser, self.method_key)
+                                        except:
+                                            continue
+                                    rewriter.apply()
 
     def __reformat(self, src_class: Class):
         subprocess.call(["java", "-jar", self.formatter, "--replace", src_class.filename])
