46f582ba53bb467239138b49406483bf12b2aad7
arisaghafari <arisa_ghafari@outlook.com>
2021-06-25 22:00:42 +0430

    Cyclic inheritance hierarchy in Java

--- smells/cyclic_hierarchy.py
+++ smells/cyclic_hierarchy.py
@@ -10,13 +10,19 @@ class CyclicHierarchy(object):
 
     def check(self):
         # package = self.program.packages
+        super_classes = []
         for package_item in self.program.packages:
             package_item_dic = self.program.packages[package_item]
             for classes_item in package_item_dic.classes:
                 class_item_dic = package_item_dic.classes[classes_item]
-                if class_item_dic.superclass_name:
+                while class_item_dic.superclass_name:
                     super_class = self.find_super_type(class_item_dic.superclass_name)
-                    a = 2
+                    super_classes.append(super_class.package_name + '.' + super_class.name)
+                    cycle = self.cycle_check(super_classes)
+                    class_item_dic = super_class
+                    if cycle:
+                        print("Find : cyclic inheritance hierarchy...")
+                        return
 
     def find_super_type(self, class_name):
         for package_item in self.program.packages:
@@ -25,6 +31,12 @@ class CyclicHierarchy(object):
                 class_item_dic = package_item_dic.classes[classes_item]
                 if class_item_dic.name == class_name:
                     return class_item_dic
+
+    def cycle_check(self, list):
+        if len(list) == len(set(list)):
+            return False
+        else:
+            return True
 
 if __name__ == "__main__":
     mylist = get_filenames_in_dir('C:/Users/Qafari/Desktop/propagationTest')
