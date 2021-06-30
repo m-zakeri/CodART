ab585b34c666cc679bdcef6bf602bef58a9dca09
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2021-06-30 11:59:09 +0430

    Rewrite changes in same input file!

--- refactorings/replace_constructor_with_factory_function.py
+++ refactorings/replace_constructor_with_factory_function.py
@@ -4,6 +4,7 @@ from antlr4.TokenStreamRewriter import TokenStreamRewriter
 from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
 from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
 from gen.javaLabeled.JavaLexer import JavaLexer
+from refactorings.utils.utils2 import get_filenames_in_dir
 
 
 class ReplaceConstructorWithFactoryFunctionRefactoringListener(JavaParserLabeledListener):
@@ -28,13 +29,13 @@ class ReplaceConstructorWithFactoryFunctionRefactoringListener(JavaParserLabeled
         self.new_parameters_names = []
 
     def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
-        #self.target_class = ctx.IDENTIFIER().getText()
-        #have_constructor = False
-        #if ctx.IDENTIFIER().getText() ==
+        # self.target_class = ctx.IDENTIFIER().getText()
+        # have_constructor = False
+        # if ctx.IDENTIFIER().getText() ==
         class_identifier = ctx.IDENTIFIER().getText()
         if class_identifier == self.target_class:
             self.is_target_class = True
-            #print("class name " + ctx.IDENTIFIER().getText())
+            # print("class name " + ctx.IDENTIFIER().getText())
         else:
             self.is_target_class = False
 
@@ -44,13 +45,13 @@ class ReplaceConstructorWithFactoryFunctionRefactoringListener(JavaParserLabeled
 
     def enterConstructorDeclaration(self, ctx: JavaParserLabeled.ConstructorDeclarationContext):
         if self.is_target_class:
-            #print("constructor name " + ctx.IDENTIFIER().getText())
-            #parameters = ctx.formalParameters().getText()
-            #print(len(ctx.formalParameters().formalParameterList().formalParameter()))
+            # print("constructor name " + ctx.IDENTIFIER().getText())
+            # parameters = ctx.formalParameters().getText()
+            # print(len(ctx.formalParameters().formalParameterList().formalParameter()))
             grandParentCtx = ctx.parentCtx.parentCtx
             if ctx.IDENTIFIER().getText() == self.target_class:
                 self.have_constructor = True
-                #do refactor
+                # do refactor
                 """
                 Declare the constructor private.
                 """
@@ -74,22 +75,24 @@ class ReplaceConstructorWithFactoryFunctionRefactoringListener(JavaParserLabeled
             grandParentCtx = ctx.parentCtx.parentCtx
             self.codeRewrite.insertAfter(
                 index=grandParentCtx.stop.tokenIndex,
-                text="\n    public static " + ctx.IDENTIFIER().getText() + " Create( " + ", ".join(self.new_parameters) +
-                     "){\n       return new " + ctx.IDENTIFIER().getText() + "(" +  ", ".join(self.new_parameters_names) + ")}"
+                text="\n    public static " + ctx.IDENTIFIER().getText() + " Create( " + ", ".join(
+                    self.new_parameters) +
+                     "){\n       return new " + ctx.IDENTIFIER().getText() + "(" + ", ".join(
+                    self.new_parameters_names) + ");\n}"
             )
         self.new_parameters = []
         self.new_parameters_names = []
 
-    def enterFormalParameterList0(self, ctx:JavaParserLabeled.FormalParameterList0Context):
-        #print(len(ctx.formalParameter()))
+    def enterFormalParameterList0(self, ctx: JavaParserLabeled.FormalParameterList0Context):
+        # print(len(ctx.formalParameter()))
         pass
 
-    def exitFormalParameterList0(self, ctx:JavaParserLabeled.FormalParameterList0Context):
+    def exitFormalParameterList0(self, ctx: JavaParserLabeled.FormalParameterList0Context):
         pass
 
-    def enterFormalParameter(self, ctx:JavaParserLabeled.FormalParameterContext):
-        #print(ctx.typeType().getText())
-        #print(ctx.variableDeclaratorId().getText())
+    def enterFormalParameter(self, ctx: JavaParserLabeled.FormalParameterContext):
+        # print(ctx.typeType().getText())
+        # print(ctx.variableDeclaratorId().getText())
         constructorName = ctx.parentCtx.parentCtx.parentCtx.IDENTIFIER().getText()
         if self.target_class == constructorName:
             text = ctx.typeType().getText() + " " + ctx.variableDeclaratorId().getText()
@@ -99,36 +102,37 @@ class ReplaceConstructorWithFactoryFunctionRefactoringListener(JavaParserLabeled
     def exitFormalParameter(self, ctx: JavaParserLabeled.FormalParameterContext):
         pass
 
-    def enterExpression4(self, ctx:JavaParserLabeled.Expression4Context):
+    def enterExpression4(self, ctx: JavaParserLabeled.Expression4Context):
         """
             Replace all constructor calls with calls to the factory method.
         """
-        #currentMethodOrClassCtx=ctx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx
-        #print(ctx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.getText())
+        # currentMethodOrClassCtx=ctx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx
+        # print(ctx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.parentCtx.getText())
         if ctx.creator().createdName().getText() == self.target_class:
             self.codeRewrite.replaceRange(
                 from_idx=ctx.start.tokenIndex,
                 to_idx=ctx.stop.tokenIndex,
-                text= self.target_class + "." + "Create" + ctx.creator().classCreatorRest().getText())
+                text=self.target_class + "." + "Create" + ctx.creator().classCreatorRest().getText())
 
 
 def main():
-    #folder_path = "..\\benchmark_projects\\JSON\\src\\main\\java\\org\\json"
-    folder_path = "..\\tests\\replace_constructor_with_factory_function_tests"
-    path = os.listdir(folder_path)
-    #target_class = "CDL"
-    target_class = "Employee"
+    # folder_path = "..\\benchmark_projects\\JSON\\src\\main\\java\\org\\json"
+    folder_path = "/data/Dev/JavaSample/"
+    path = get_filenames_in_dir(folder_path)
+    print(path)
+    # target_class = "CDL"
+    target_class = "ReplaceConstructorWithFactoryMethod"
 
     for file in path:
         if file.endswith('.java') and not file.endswith('_refactored.java'):
-            each = folder_path + "\\" + file
-            stream = FileStream(str(each))
+            stream = FileStream(file)
             lexer = JavaLexer(stream)
             tokens = CommonTokenStream(lexer)
             parser = JavaParserLabeled(tokens)
             tree = parser.compilationUnit()
-            new_file = open(os.path.join(folder_path, file + "_refactored.java"), mode='w', newline='')
-            listener = ReplaceConstructorWithFactoryFunctionRefactoringListener(common_token_stream=tokens, target_class=target_class)
+            new_file = open(file, mode='w', newline='')
+            listener = ReplaceConstructorWithFactoryFunctionRefactoringListener(common_token_stream=tokens,
+                                                                                target_class=target_class)
             walker = ParseTreeWalker()
             walker.walk(
                 listener=listener,
