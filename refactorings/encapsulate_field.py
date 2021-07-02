d9a5e8da1d7326b8f1ac7aa97e17db9edb4b33bf
sadra goudarzdashti <s.goudarzdashti@gmail.com>
2021-07-02 12:51:35 +0430

    resolve review issues

--- refactorings/encapsulate_field.py
+++ refactorings/encapsulate_field.py
@@ -29,140 +29,175 @@ class EncapsulateFiledRefactoringListener(JavaParserLabeledListener):
     """
 #
     def __init__(self, common_token_stream: CommonTokenStream = None,
+                 source_class_name: str = None,
                  field_identifier: str = None):
         """
         :param common_token_stream:
         """
         self.token_stream = common_token_stream
+        self.source_class_name = source_class_name
         self.field_identifier = field_identifier
         self.getter_exist = False
         self.setter_exist = False
+        self.in_source_class = False
         # Move all the tokens in the source code in a buffer, token_stream_rewriter.
         if common_token_stream is not None:
-            self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
+            self.token_stream_rewriter =\
+                TokenStreamRewriter(common_token_stream)
         else:
             raise TypeError('common_token_stream is None')
 
+    def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
+        if ctx.IDENTIFIER().getText() == self.source_class_name:
+            self.in_source_class = True
+
+    def exitClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
+        self.in_source_class = False
+
     def exitFieldDeclaration(self, ctx: JavaParserLabeled.FieldDeclarationContext):
-        if ctx.variableDeclarators().getText() == self.field_identifier:
-            if not ctx.parentCtx.parentCtx.modifier(0):
-                self.token_stream_rewriter.insertBeforeIndex(
-                     index=ctx.typeType().stop.tokenIndex,
-                     text='private ')
+        if self.in_source_class:
+            if ctx.variableDeclarators().getText() == self.field_identifier:
+                if not ctx.parentCtx.parentCtx.modifier(0):
+                    self.token_stream_rewriter.insertBeforeIndex(
+                        index=ctx.typeType().stop.tokenIndex,
+                        text='private ')
 
-            elif ctx.parentCtx.parentCtx.modifier(0).getText() == 'public':
-                self.token_stream_rewriter.replaceRange(
-                    from_idx=ctx.parentCtx.parentCtx.modifier(0).start.tokenIndex,
-                    to_idx=ctx.parentCtx.parentCtx.modifier(0).stop.tokenIndex,
-                    text='private')
-            else:
-                return
+                elif ctx.parentCtx.parentCtx.modifier(0).getText() == 'public':
+                    self.token_stream_rewriter.replaceRange(
+                        from_idx=ctx.parentCtx.parentCtx.modifier(0).start.tokenIndex,
+                        to_idx=ctx.parentCtx.parentCtx.modifier(0).stop.tokenIndex,
+                        text='private')
+                else:
+                    return
 
-            for c in ctx.parentCtx.parentCtx.parentCtx.classBodyDeclaration():
-                try:
-                    print('method name: ' + c.memberDeclaration().methodDeclaration().IDENTIFIER().getText())
-                    if c.memberDeclaration().methodDeclaration().IDENTIFIER().getText() == 'get' + str.capitalize(self.field_identifier):
-                        self.getter_exist = True
-                    if c.memberDeclaration().methodDeclaration().IDENTIFIER().getText() == 'set' + str.capitalize(self.field_identifier):
-                        self.setter_exist = True
-                except :
-                    print("not method !!!")
+                for c in ctx.parentCtx.parentCtx.parentCtx.classBodyDeclaration():
+                    try:
+                        print('method name: ' + c.memberDeclaration()
+                              .methodDeclaration().IDENTIFIER().getText())
 
-            print("setter find: " + str(self.setter_exist))
-            print("getter find: " + str(self.getter_exist))
+                        if c.memberDeclaration().methodDeclaration().IDENTIFIER()\
+                                .getText() == 'get' + str.capitalize(
+                                self.field_identifier):
+                            self.getter_exist = True
 
-            # generate accessor and mutator methods
-            # Accessor body
-            new_code = ''
-            if not self.getter_exist:
-                new_code = '\n\t// new getter method\n\t'
-                new_code += 'public ' + ctx.typeType().getText() + ' get' + str.capitalize(self.field_identifier)
-                new_code += '() { \n\t\treturn this.' + self.field_identifier + ';' + '\n\t}\n'
+                        if c.memberDeclaration().methodDeclaration().IDENTIFIER()\
+                                .getText() == 'set' + str.capitalize(
+                                self.field_identifier):
+                            self.setter_exist = True
 
-            # Mutator body
-            if not self.setter_exist:
-                new_code += '\n\t// new setter method\n\t'
-                new_code += 'public void set' + str.capitalize(self.field_identifier)
-                new_code += '(' + ctx.typeType().getText() + ' ' + self.field_identifier + ') { \n\t\t'
-                new_code += 'this.' + self.field_identifier + ' = ' + self.field_identifier + ';' + '\n\t}\n'
+                    except:
+                        print("not method !!!")
 
-            self.token_stream_rewriter.insertAfter(ctx.stop.tokenIndex, new_code)
+                print("setter find: " + str(self.setter_exist))
+                print("getter find: " + str(self.getter_exist))
 
-            hidden = self.token_stream.getHiddenTokensToRight(ctx.stop.tokenIndex)
-            # self.token_stream_rewriter.replaceRange(from_idx=hidden[0].tokenIndex,
-            #                                         to_idx=hidden[-1].tokenIndex,
-            #                                         text='\n\t/*End of accessor and mutator methods!*/\n\n')
+                # generate accessor and mutator methods
+                # Accessor body
+                new_code = ''
+                if not self.getter_exist:
+                    new_code = '\n\t// new getter method\n\t'
+                    new_code += 'public ' + ctx.typeType().getText() + \
+                                ' get' + str.capitalize(self.field_identifier)
+                    new_code += '() { \n\t\treturn this.' + self.field_identifier\
+                                + ';' + '\n\t}\n'
 
-    def exitExpression21(self, ctx:JavaParserLabeled.Expression21Context):
-        # print("test: ",ctx.expression(0).IDENTIFIER().getText())
+                # Mutator body
+                if not self.setter_exist:
+                    new_code += '\n\t// new setter method\n\t'
+                    new_code += 'public void set' + str.capitalize(self.field_identifier)
+                    new_code += '(' + ctx.typeType().getText() + ' ' \
+                                + self.field_identifier + ') { \n\t\t'
+                    new_code += 'this.' + self.field_identifier + ' = '\
+                                + self.field_identifier + ';' + '\n\t}\n'
 
-        if ctx.expression(0).getText() == self.field_identifier or \
-                ctx.expression(0).getText() == 'this.' + self.field_identifier:
-            expr_code = self.token_stream_rewriter.getText(program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
-                                                           start=ctx.expression(1).start.tokenIndex,
-                                                           stop=ctx.expression(1).stop.tokenIndex)
-            # new_code = 'this.set' + str.capitalize(self.field_identifier) + '(' + ctx.expression().getText() + ')'
-            new_code = 'this.set' + str.capitalize(self.field_identifier) + '(' + expr_code + ')'
-            self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex, ctx.stop.tokenIndex, new_code)
-#
-#
-    def exitExpression0(self, ctx:JavaParserLabeled.Expression0Context):
-        # if ctx.getChildCount() == 2:
-        try:
-            if ctx.parentCtx.getChild(1).getText() in ('=','+=', '-=', '*=', '/=',
-                                                       '&=', '|=', '^=', '>>=',
-                                                       '>>>=', '<<=', '%=') and \
-                    ctx.parentCtx.getChild(0) == ctx:
-                return
-        except:
-            pass
-        if ctx.getText() == self.field_identifier:
-            new_code = 'this.get' + str.capitalize(self.field_identifier) + '()'
-            self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex, ctx.stop.tokenIndex, new_code)
+                self.token_stream_rewriter.insertAfter(ctx.stop.tokenIndex, new_code)
 
-    def exitExpression1(self, ctx:JavaParserLabeled.Expression1Context):
-        # if ctx.getChildCount() == 2:
-        try:
-            if ctx.parentCtx.getChild(1).getText() in ('=','+=', '-=', '*=', '/=',
-                                                       '&=', '|=', '^=', '>>=',
-                                                       '>>>=', '<<=', '%=') and \
-                    ctx.parentCtx.getChild(0) == ctx:
-                return
-        except:
-            pass
-        if ctx.getText() == 'this.' + self.field_identifier:
-            new_code = 'this.get' + str.capitalize(self.field_identifier) + '()'
-            self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex, ctx.stop.tokenIndex, new_code)
+                hidden = self.token_stream.getHiddenTokensToRight(ctx.stop.tokenIndex)
+                # self.token_stream_rewriter.replaceRange(from_idx=hidden[0].tokenIndex,
+                #                                         to_idx=hidden[-1].tokenIndex,
+                #                                         text='\n\t/*End of accessor and mutator methods!*/\n\n')
+
+    def exitExpression21(self, ctx: JavaParserLabeled.Expression21Context):
+        if self.in_source_class:
+            if ctx.expression(0).getText() == self.field_identifier or \
+                    ctx.expression(0).getText() == 'this.' + self.field_identifier:
+                expr_code = self.token_stream_rewriter.getText(
+                    program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                    start=ctx.expression(1).start.tokenIndex,
+                    stop=ctx.expression(1).stop.tokenIndex)
+                new_code = 'this.set' + str.capitalize(self.field_identifier) + '(' + expr_code + ')'
+                self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex, ctx.stop.tokenIndex, new_code)
+
+    def exitExpression0(self, ctx: JavaParserLabeled.Expression0Context):
+        if self.in_source_class:
+            try:
+                if ctx.parentCtx.getChild(1).getText() in ('=', '+=', '-=',
+                                                           '*=', '/=', '&=',
+                                                           '|=', '^=', '>>=',
+                                                           '>>>=', '<<=', '%=') and \
+                        ctx.parentCtx.getChild(0) == ctx:
+                    return
+            except:
+                pass
+            if ctx.getText() == self.field_identifier:
+                new_code = 'this.get' + str.capitalize(self.field_identifier) + '()'
+                self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex,
+                                                        ctx.stop.tokenIndex,
+                                                        new_code)
+
+    def exitExpression1(self, ctx: JavaParserLabeled.Expression1Context):
+        if self.in_source_class:
+            try:
+                if ctx.parentCtx.getChild(1).getText() in ('=', '+=', '-=',
+                                                           '*=', '/=', '&=',
+                                                           '|=', '^=', '>>=',
+                                                           '>>>=', '<<=', '%=') and \
+                        ctx.parentCtx.getChild(0) == ctx:
+                    return
+            except:
+                pass
+            if ctx.getText() == 'this.' + self.field_identifier:
+                new_code = 'this.get' + str.capitalize(self.field_identifier) + '()'
+                self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex,
+                                                        ctx.stop.tokenIndex,
+                                                        new_code)
 
     def enterCompilationUnit(self, ctx: JavaParserLabeled.CompilationUnitContext):
-        hidden = self.token_stream.getHiddenTokensToLeft(ctx.start.tokenIndex)
-        self.token_stream_rewriter.replaceRange(from_idx=hidden[0].tokenIndex,
-                                                to_idx=hidden[-1].tokenIndex,
-                                                text='/*After refactoring (Refactored version)*/\n')
+        try:
+            hidden = self.token_stream.getHiddenTokensToLeft(ctx.start.tokenIndex)
+            self.token_stream_rewriter.replaceRange(from_idx=hidden[0].tokenIndex,
+                                                    to_idx=hidden[-1].tokenIndex,
+                                                    text='/*After refactoring (Refactored version)*/\n')
+        except:
+            pass
 
 
+def main(directory_path, source_class, field_name):
+    print('Encapsulate Field')
 
-def main():
-    Path = "../tests/encapsulate_field_tests/"
-    test_file = FileStream(str(Path + "use_field_propagation_test.java"))
-    print("file opened")
-    Refactored = open(os.path.join(Path, "use_field_propagation_test_refactored.java"), 'w', newline='')
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith('.java'):
+                stream = FileStream(os.path.join(root, file), encoding='utf8')
+                lexer = JavaLexer(stream)
+                token_stream = CommonTokenStream(lexer)
+                parser = JavaParserLabeled(token_stream)
+                ef_listener = EncapsulateFiledRefactoringListener(token_stream,
+                                                                  source_class,
+                                                                  field_name)
+                tree = parser.compilationUnit()
+                walker = ParseTreeWalker()
+                walker.walk(t=tree, listener=ef_listener)
 
-    Lexer = JavaLexer(test_file)
+                refactored = open(os.path.join(root, file), 'w', newline='')
+                refactored.write(ef_listener.token_stream_rewriter.getDefaultText())
+        break
 
-    TokenStream = CommonTokenStream(Lexer)
-
-    Parser = JavaParserLabeled(TokenStream)
-
-    EFlistener = EncapsulateFiledRefactoringListener(TokenStream, 'f')
-    Tree = Parser.compilationUnit()
-
-    Walker = ParseTreeWalker()
-    Walker.walk(t=Tree, listener=EFlistener)
-
-    Refactored.write(EFlistener.token_stream_rewriter.getDefaultText())
-    print("finished!!!!!!!!")
+    print('Finished!')
 
 
 if __name__ == "__main__":
-    main()
+    directory_path = "../tests/encapsulate_field_tests/"
+    source_class = 'A'
+    field_name = 'f'
+    main(directory_path, source_class, field_name)
