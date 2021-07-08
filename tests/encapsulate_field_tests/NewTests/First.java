4334a87e1a6ec1f122c4bd15e11193f12ec6fb1d
sadra goudarzdashti <s.goudarzdashti@gmail.com>
2021-07-08 18:38:09 +0430

    fix new tests

--- tests/encapsulate_field_tests/NewTests/First.java
+++ tests/encapsulate_field_tests/NewTests/First.java
@@ -1,17 +1,7 @@
 package learnjava;
 
 public class First{
-    private int score;
-	// new getter method
-	public int getScore() { 
-		return this.score;
-	}
-
-	// new setter method
-	public void setScore(int score) { 
-		this.score = score;
-	}
-
+    public int score;
     public void msg() {
         System.out.println("Hello");
     }
--- tests/encapsulate_field_tests/NewTests/Second.java
+++ tests/encapsulate_field_tests/NewTests/Second.java
@@ -1,12 +1,12 @@
 package testjava;
 
-import learnjava.*;
+import learnjava.;
 
 class Second {  
     public static void main(String[] args) {  
         First obj = new First();
 	    obj.msg();
-	    obj.setScore(12);
-    	System.out.println(obj.getScore());
+	    obj.score = 12;
+    	System.out.println(obj.score);
     }  
 }
