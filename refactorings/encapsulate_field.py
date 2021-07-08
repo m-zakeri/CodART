4e9cb5b022762f28d6140826e21b9e2731ac15d9
sadra goudarzdashti <s.goudarzdashti@gmail.com>
2021-07-08 18:10:07 +0430

    add new listener for propagate changes on instances and walk tree twice

--- refactorings/encapsulate_field.py
+++ refactorings/encapsulate_field.py
@@ -27,7 +27,8 @@ class EncapsulateFiledRefactoringListener(JavaParserLabeledListener):
     To implement the encapsulate filed refactored
     Encapsulate field: Make a public field private and provide accessors
     """
-#
+
+    #
     def __init__(self, common_token_stream: CommonTokenStream = None,
                  package_name: str = None,
                  source_class_name: str = None,
@@ -48,12 +49,12 @@ class EncapsulateFiledRefactoringListener(JavaParserLabeledListener):
         self.in_selected_package = True if self.package_name == '' else False
         # Move all the tokens in the source code in a buffer, token_stream_rewriter.
         if common_token_stream is not None:
-            self.token_stream_rewriter =\
+            self.token_stream_rewriter = \
                 TokenStreamRewriter(common_token_stream)
         else:
             raise TypeError('common_token_stream is None')
 
-    def enterPackageDeclaration(self, ctx:JavaParserLabeled.PackageDeclarationContext):
+    def enterPackageDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext):
         if self.package_name == ctx.qualifiedName().getText():
             self.in_selected_package = True
         else:
@@ -87,14 +88,14 @@ class EncapsulateFiledRefactoringListener(JavaParserLabeledListener):
                         print('method name: ' + c.memberDeclaration()
                               .methodDeclaration().IDENTIFIER().getText())
 
-                        if c.memberDeclaration().methodDeclaration().IDENTIFIER()\
+                        if c.memberDeclaration().methodDeclaration().IDENTIFIER() \
                                 .getText() == 'get' + str.capitalize(
-                                self.field_identifier):
+                            self.field_identifier):
                             self.getter_exist = True
 
-                        if c.memberDeclaration().methodDeclaration().IDENTIFIER()\
+                        if c.memberDeclaration().methodDeclaration().IDENTIFIER() \
                                 .getText() == 'set' + str.capitalize(
-                                self.field_identifier):
+                            self.field_identifier):
                             self.setter_exist = True
 
                     except:
@@ -110,7 +111,7 @@ class EncapsulateFiledRefactoringListener(JavaParserLabeledListener):
                     new_code = '\n\t// new getter method\n\t'
                     new_code += 'public ' + ctx.typeType().getText() + \
                                 ' get' + str.capitalize(self.field_identifier)
-                    new_code += '() { \n\t\treturn this.' + self.field_identifier\
+                    new_code += '() { \n\t\treturn this.' + self.field_identifier \
                                 + ';' + '\n\t}\n'
 
                 # Mutator body
@@ -119,9 +120,8 @@ class EncapsulateFiledRefactoringListener(JavaParserLabeledListener):
                     new_code += 'public void set' + str.capitalize(self.field_identifier)
                     new_code += '(' + ctx.typeType().getText() + ' ' \
                                 + self.field_identifier + ') { \n\t\t'
-                    new_code += 'this.' + self.field_identifier + ' = '\
+                    new_code += 'this.' + self.field_identifier + ' = ' \
                                 + self.field_identifier + ';' + '\n\t}\n'
-
                 self.token_stream_rewriter.insertAfter(ctx.stop.tokenIndex, new_code)
 
                 hidden = self.token_stream.getHiddenTokensToRight(ctx.stop.tokenIndex)
@@ -184,6 +184,79 @@ class EncapsulateFiledRefactoringListener(JavaParserLabeledListener):
             pass
 
 
+class InstancePropagationEncapsulateFieldListener(JavaParserLabeledListener):
+    def __init__(self, token_stream_rewriter: TokenStreamRewriter = None,
+                 package_name: str = None,
+                 source_class_name: str = None,
+                 field_identifier: str = None):
+        if package_name is None:
+            self.package_name = ''
+        else:
+            self.package_name = package_name
+        self.source_class_name = source_class_name
+        self.field_identifier = field_identifier
+        self.has_access_to_class = False
+        self.instances = list()
+        # Move all the tokens in the source code in a buffer, token_stream_rewriter.
+        if token_stream_rewriter is not None:
+            self.token_stream_rewriter = \
+                token_stream_rewriter
+        else:
+            raise TypeError('common_token_stream is None')
+
+    def enterPackageDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext):
+        if self.package_name == ctx.qualifiedName().getText():
+            self.has_access_to_class = True
+
+    def enterImportDeclaration(self, ctx: JavaParserLabeled.ImportDeclarationContext):
+        if ctx.qualifiedName().getText() == self.package_name + '.' + self.source_class_name \
+                or ctx.qualifiedName().getText() == self.package_name:
+            self.has_access_to_class = True
+
+    def exitLocalVariableDeclaration(self, ctx: JavaParserLabeled.LocalVariableDeclarationContext):
+        try:
+            if (self.has_access_to_class and ctx.variableDeclarators().variableDeclarator(0) \
+                    .variableInitializer().expression().creator() \
+                    .createdName().getText() == self.source_class_name) \
+                    or ctx.variableDeclarators().variableDeclarator(0) \
+                    .variableInitializer().expression().creator() \
+                    .createdName().getText() == self.package_name + '.' + self.source_class_name:
+                self.instances.append(ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText())
+        except:
+            pass
+
+    def exitMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
+        self.instances = []
+
+    def exitExpression1(self, ctx: JavaParserLabeled.Expression1Context):
+        try:
+            if ctx.parentCtx.getChild(1).getText() in ('=', '+=', '-=',
+                                                       '*=', '/=', '&=',
+                                                       '|=', '^=', '>>=',
+                                                       '>>>=', '<<=', '%=') and \
+                    ctx.parentCtx.getChild(0) == ctx:
+                return
+        except:
+            pass
+
+        for instance in self.instances:
+            if ctx.getText() == instance + '.' + self.field_identifier:
+                new_code = instance + '.get' + str.capitalize(self.field_identifier) + '()'
+                self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex,
+                                                        ctx.stop.tokenIndex,
+                                                        new_code)
+
+    def exitExpression21(self, ctx: JavaParserLabeled.Expression21Context):
+        for instance in self.instances:
+            if ctx.expression(0).getText() == instance + '.' + self.field_identifier:
+                expr_code = self.token_stream_rewriter.getText(
+                    program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                    start=ctx.expression(1).start.tokenIndex,
+                    stop=ctx.expression(1).stop.tokenIndex)
+                new_code = instance + '.set' + str.capitalize(self.field_identifier) + '(' + expr_code + ')'
+                self.token_stream_rewriter.replaceRange(ctx.start.tokenIndex, ctx.stop.tokenIndex, new_code)
+
+
 def main(directory_path, package_name, source_class, field_name):
     print('Encapsulate Field')
 
@@ -202,16 +275,23 @@ def main(directory_path, package_name, source_class, field_name):
                 walker = ParseTreeWalker()
                 walker.walk(t=tree, listener=ef_listener)
 
+                ip_listener = InstancePropagationEncapsulateFieldListener(ef_listener.token_stream_rewriter,
+                                                                  package_name,
+                                                                  source_class,
+                                                                  field_name)
+                walker.walk(t=tree, listener=ip_listener)
+
                 refactored = open(os.path.join(root, file), 'w', newline='')
-                refactored.write(ef_listener.token_stream_rewriter.getDefaultText())
+                refactored.write(ip_listener.token_stream_rewriter.getDefaultText())
+                refactored.close()
         break
 
     print('Finished!')
 
 
 if __name__ == "__main__":
-    directory_path = "../tests/encapsulate_field_tests/"
-    package_name = ''
-    source_class = 'A'
-    field_name = 'f'
+    directory_path = "../tests/encapsulate_field_tests/NewTests"
+    package_name = 'learnjava'
+    source_class = 'First'
+    field_name = 'score'
     main(directory_path, package_name, source_class, field_name)
