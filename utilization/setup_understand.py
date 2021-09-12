001d31b2a6f9886ca5a85f211e74a6c993b6217e
Seyyed Ali Ayati <ayati_a@comp.iust.ac.ir>
2021-09-12 10:52:51 +0430

    Added .env support to utilization

--- utilization/setup_understand.py
+++ utilization/setup_understand.py
@@ -5,28 +5,31 @@ You should set the path of understand installation in this script
 """
 import os
 import sys
+import logging
 
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__file__)
+load_dotenv()
 
 # -------------------
 # For Linux os
-LD_LIBRARY_PATH = "/home/ali/scitools/bin/linux64/"  # Put your path here
-PYTHONPATH = "/home/ali/scitools/bin/linux64/Python"  # Put your path here
-
-os.environ["LD_LIBRARY_PATH"] = LD_LIBRARY_PATH  # Put your path here
-
+PYTHONPATH = os.environ.get("PYTHONPATH")  # Put your path here
 
 # -------------------
 # For Windows os
 # https://scitools.com/support/python-api/
 # Python 3.8 and newer require the user add a call to os.add_dll_directory(“SciTools/bin/“  # Put your path here
 # os.add_dll_directory('C:/Program Files/SciTools/bin/pc-win64')  # Put your path here
-sys.path.insert(0, 'D:/program files/scitools/bin/pc-win64/python')  # Put your path here
-
+sys.path.insert(0, PYTHONPATH)  # Put your path here
 
 # --------------------
 # Import understand if available on the path
 try:
     import understand as und
+    logger.info(f"Loaded understand {und.version()} successfully")
 except ModuleNotFoundError:
-    raise ModuleNotFoundError('Understand cannot import')
-
+    raise ModuleNotFoundError('Understand not found.')
+except ImportError:
+    raise ImportError("Can not import")
