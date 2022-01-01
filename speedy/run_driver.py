f6d9974f4b14d2be364ec689e01af88f3e706aaf
m-zakeri <m-zakeri@live.com>
2022-01-01 19:18:43 +0330

    Update test cases

--- speedy/run_driver.py
+++ speedy/run_driver.py
@@ -4,27 +4,29 @@ Benchmark driver for CodeART C++ backend
 """
 
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __author__ = 'Morteza'
 
-
-import java8speedy
-from java8speedy.parser import sa_java
-from java8speedy.benchmark import benchmark
 import antlr4
-# file_stream_ = antlr4.FileStream('../tests/utils_test2.java')
-# sa_java._cpp_parse(file_stream_, 'compilationUnit')
+import java8speedy
+from java8speedy.parser import sa_javalabeled
+from java8speedy.benchmark import benchmark
 
 
-# quit()
 
-# java9speedy.print_tree('../refactorings/input.java')
-# java9speedy.benchmark('../refactorings/input.java', 1000)
-# java9speedy.print_tree('Test_WekaPackageManager.java')
-# benchmark('Test_WekaPackageManager.java', 1)
-benchmark('../tests/utils_test2.java')
+if __name__ == '__main__':
+    if sa_javalabeled.USE_CPP_IMPLEMENTATION:
+        print("JAVA 8 Using C++ implementation of parser")
+    else:
+        print("Using Python implementation of parser")
 
-if sa_java.USE_CPP_IMPLEMENTATION:
-    print("JAVA 8 Using C++ implementation of parser")
-else:
-    print("Using Python implementation of parser")
+    file_stream = antlr4.FileStream('../tests/utils_test2.java')
+    file_stream = antlr4.FileStream('Test_WekaPackageManager.java')
+    sa_javalabeled._cpp_parse(file_stream, 'compilationUnit')
+
+    # java9speedy.print_tree('../refactorings/input.java')
+    # java9speedy.benchmark('../refactorings/input.java', 1000)
+    # java9speedy.print_tree('Test_WekaPackageManager.java')
+
+    benchmark('Test_WekaPackageManager.java', 5)
+    # benchmark('../tests/utils_test2.java')
