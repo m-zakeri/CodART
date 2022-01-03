7aa0c04288df4895e9012cd94e469053d2b9d29e
m-zakeri <m-zakeri@live.com>
2022-01-03 18:31:42 +0330

    Update test

--- tests/utils_test2.java
+++ tests/utils_test2.java
@@ -2,7 +2,18 @@ package tests.utils_test2;
 
 public class Test4
 {
+    int x;
 }
-public class Test3
+
+public class Test1
 {
+    private int a;
+    private float b;
+
+    void F(int x, int y)
+    {
+        int x = 0;
+        int y = 1;
+    }
+
 }
