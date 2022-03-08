06095bfd88c0e2439778f478b88ab0712d66df61
m-zakeri <m-zakeri@live.com>
2022-03-08 22:58:29 +0330

    Improve modularity computation performance

--- metrics/modularity.py
+++ metrics/modularity.py
@@ -10,33 +10,72 @@ to be used in refactoring process in addition to qmood metrics
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Morteza Zakeri'
 
 import os
+import re
 import time
-import logging
+from collections import defaultdict
 
-logger = logging.getLogger()
-
+import pandas
 import networkx as nx
 import networkx.algorithms.community as nx_comm
-import pandas
-import understand
 from matplotlib import pyplot as plt
 
-from metrics.metrics_coverability import UnderstandUtility
+import understand
+
 from codart.utility.directory_utils import export_understand_dependencies_csv
 
 
 class Modularity:
-    def __init__(self, graph_path, db, **kwargs):
+    def __init__(self, graph_path, project_db_path, **kwargs):
         self.mdg_df = pandas.read_csv(graph_path)
-        self.mdg_graph = nx.from_pandas_edgelist(self.mdg_df, source='From Class', target='To Class',
-                                                 edge_attr='References', create_using=nx.DiGraph())
-        self.db = db
+        self.project_db_path = project_db_path
+        # Delete nested classes
+        # Dropping the rows of "(" or ")"
+        print('Before delete nested classes', self.mdg_df.shape)
+        self.mdg_df = self.mdg_df[~self.mdg_df["From Class"].str.contains(r"\)")]
+        self.mdg_df = self.mdg_df[~self.mdg_df["To Class"].str.contains(r"\)")]
+        print('After delete nested classes', self.mdg_df.shape)
+        self.class_package_dict = dict()
+        self.create_class_package_dict()
 
-        # self.show_mdg()
+        self.mdg_graph = nx.from_pandas_edgelist(
+            self.mdg_df, source='From Class',
+            target='To Class',
+            edge_attr='References',
+            create_using=nx.DiGraph()
+        )
+
+
+    def create_class_package_dict(self):
+        classes = []
+        classes.extend(self.mdg_df["From Class"].values)
+        classes.extend(self.mdg_df["To Class"].values)
+        classes = set(classes)
+        # print(len(classes), )
+        # print(classes)
+        db = understand.open(self.project_db_path)
+        for class_longname in classes:
+            # print('Processing ',class_longname )
+            entities = db.lookup(re.compile(class_longname + r'$'), )
+            if entities is None or len(entities) == 0:  # Nested classes
+                self.mdg_df = self.mdg_df[~self.mdg_df["From Class"].str.contains(class_longname)]
+                self.mdg_df = self.mdg_df[~self.mdg_df["To Class"].str.contains(class_longname)]
+            else:
+                class_entity = entities[0]
+                package_list = class_entity.ents('Containin', 'Java Package')
+                while not package_list and class_entity.parent() is not None:
+                    package_list = class_entity.parent().ents('Containin', 'Java Package')
+                    class_entity = class_entity.parent()
+                # print(package_list)
+                if len(package_list) < 1:
+                    self.class_package_dict.update({class_longname: 'default'})
+                else:
+                    self.class_package_dict.update({class_longname: package_list[0].longname()})
+        db.close()
+
 
     def show_mdg(self):
         pos = nx.spring_layout(self.mdg_graph)
@@ -57,7 +96,12 @@ class Modularity:
         """
         q = 0
         if self.mdg_graph is not None and self.mdg_graph.number_of_edges() > 0:
-            communities = self.__roster_communities()
+            # communities = self.__roster_communities()
+
+            # communities = {v: k for k, v in self.class_package_dict.items()}
+            communities = defaultdict(list)
+            for key, value in self.class_package_dict.items():
+                communities[value].append(key)
             q = nx_comm.modularity(self.mdg_graph, communities=communities.values())
         # print(q)
         return q
@@ -73,13 +117,38 @@ class Modularity:
         for node_ in self.mdg_graph:
             # print('node_:', node_)
             # package_name = self.__get_package_name_by_parsing(node_)
-            package_name = self.__get_package_name_by_understand_query(node_)
+            # package_name = self.__get_package_name_by_understand_query(node_)
+            package_name = self.class_package_dict[node_]
             if package_name in communities_dict.keys():
                 communities_dict[package_name].append(node_)
             else:
                 communities_dict[package_name] = [node_]
         # print(communities_dict.values())
         return communities_dict
+
+    def __get_package_name_by_understand_query(self, class_longname: str = None):
+        """
+        This method can be used instead of `__get_package_name_by_parsing` and it is more accurate
+
+        """
+        db = understand.open(self.project_db_path)
+        entities = db.lookup(re.compile(class_longname + r'$'), )
+        if entities is None or len(entities) == 0:  # Nested classes
+            db.close()
+            return 'default'
+        else:
+            class_entity = entities[0]
+            package_list = class_entity.ents('Containin', 'Java Package')
+            while not package_list and class_entity.parent() is not None:
+                package_list = class_entity.parent().ents('Containin', 'Java Package')
+                class_entity = class_entity.parent()
+            if len(package_list) < 1:
+                db.close()
+                return 'default'
+            else:
+                package_name = package_list[0].longname()
+                db.close()
+                return package_name
 
     def __get_package_name_by_parsing(self, class_longname: str = None):
         if class_longname.find('.') == -1:
@@ -88,41 +157,29 @@ class Modularity:
             package_name, class_short_name = class_longname.rsplit('.', 1)
             return package_name
 
-    def __get_package_name_by_understand_query(self, class_longname: str = None):
-        """
-        This method can be used instead of `__get_package_name_by_parsing` and it is more accurate
-
-        """
-        package_entity, package_longname = UnderstandUtility.get_package_of_given_class_2(self.db, class_longname)
-        # print(package_entity.longname())
-
-        return package_longname
-
 
 # Modularity API
-def main(project_path='../benchmark_projects/JSON/JSON.und', initial_value=1.0):
+def main(project_db_path=None, initial_value=1.0):
     """
     A demo of using modularity module to measure modularity quality attribute based on graph-analysis
     """
     csv_path = os.path.abspath('../metrics/mdg/MDG.csv')
     export_understand_dependencies_csv(
         csv_path=csv_path,
-        db_path=project_path
+        db_path=project_db_path
     )
     while not os.path.exists(csv_path):
-        logger.debug("Waiting 0.05s for the file to be created...")
         time.sleep(0.05)
 
-    db = understand.open(project_path)
-    modulo = Modularity(graph_path=csv_path, db=db)
+    modulo = Modularity(graph_path=csv_path, project_db_path=project_db_path)
     q = modulo.compute_modularity_newman_leicht()
     os.remove(csv_path)
-    db.close()
     return q / initial_value
 
 
 # Test module
 if __name__ == '__main__':
     from sbse.config import UDB_PATH
-    print(main(UDB_PATH))
 
+    for i in range(10):
+        print(main(UDB_PATH))
