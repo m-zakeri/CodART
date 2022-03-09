19ce64b0d1fd74c2474811051070f14f054dbf4b
m-zakeri <m-zakeri@live.com>
2022-03-09 11:30:38 +0330

    Fix initialize values

--- sbse/config.py
+++ sbse/config.py
@@ -30,59 +30,92 @@ BENCHMARK_INDEX = int(os.environ.get("BENCHMARK_INDEX", 0))
 BENCHMARKS = {
     # Project Path
     'PROJ': [
-        'tabula-java',
-        'JSON20201115',
+        '10_water-simulator',
+        '61_noen',
+        '88_jopenchart',
+        '104_vuze',  # Not ready
         '105_freemind',
         '107_weka',
+        'commons-codec',
         'ganttproject_1_11_1_original',
         'jfreechart-master',
+        'JSON20201115',
         'jvlt-1.3.2',
-        '88_jopenchart',
-        '104_vuze',
-
+        'tabula-java',
     ],
+
     # Understand DB Path
     'UDB': [
-        'udbs/tabula-java.und',
-        'JSON20201115/JSON20201115.und',
-        '105_freemind/105_freemind.und',
-        '107_weka/107_weka.und',
-        'ganttproject_1_11_1_original/ganttproject_1_11_1_original.und',
-        'jfreechart-master/jfreechart-master.und',
-        'jvlt-1.3.2/jvlt-1.3.2.und',
-        '88_jopenchart.udb',
-        '104_vuze/104_vuze.und',
+        # '10_water-simulator.udb',
+        '10_water-simulator.und',
+        # '61_noen.udb',
+        '61_noen.und',
+        # '88_jopenchart.udb',
+        '88_jopenchart.und',
+        # '104_vuze.udb',  # Not ready
+        '104_vuze.und',  # Not ready
+        # '105_freemind.udb',
+        '105_freemind.und',
+        # '107_weka.udb',
+        '107_weka.und',
+        # 'commons-codec.udb',
+        'commons-codec.und',
+        # 'ganttproject_1_11_1_original.udb',
+        'ganttproject_1_11_1_original.und',
+        # 'jfreechart-master.udb',
+        'jfreechart-master.und',
+        # 'JSON20201115.udb',
+        'JSON20201115.und',
+        # 'jvlt-1.3.2.udb',
+        'jvlt-1.3.2.und',
+        # 'tabula-java.udb',
+        'tabula-java.und',
     ],
+
     # CSV files path containing code smells identified by JDeodorant
     'LONG_METHOD': [
-        'tabula-java/fake.csv',
-        'JSON20201115/Long-Method2_JASON-20201115.csv',
-        'FreeMind-0.9.0/Long-Method2_FreeMind-0.9.0.csv',
-        'Weka-3.8/Long-Method2-Weka3.8.csv',
-        'GanttProject-1.11.1/Long-Method2_ganttproject-1.11.1.csv',
-        'JFreeChart-1.0.19/Long-Method2_JFreeChart-1.0.19.csv',
-        'jVLT-1.3.2/Long-Method2_jvlt-1.3.2.csv',
+        '10_water-simulator/fake.csv',
+        '61_noen/fake.csv',
         '88_jopenchart/fake.csv',
+        '104_vuze/fake.csv',
+        '105_freemind/Long-Method2_FreeMind-0.9.0.csv',
+        '107_weka/Long-Method2-Weka3.8.csv',
+        'commons-codec/fake.csv',
+        'ganttproject_1_11_1_original/Long-Method2_ganttproject-1.11.1.csv',
+        'jfreechart-master/Long-Method2_JFreeChart-1.0.19.csv',
+        'JSON20201115/Long-Method2_JASON-20201115.csv',
+        'jVLT-1.3.2/Long-Method2_jvlt-1.3.2.csv',
+        'tabula-java/fake.csv',
     ],
-    'GOD_CLASS': [
-        'tabula-java/God-Class-Tabula-v1.0.6.csv',
-        'JSON20201115/God-Class_JASON-20201115.csv',
-        'FreeMind-0.9.0/God-Class_FreeMind-0.9.0.csv',
-        'Weka-3.8/God-Class-Weka3.8.csv',
-        'GanttProject-1.11.1/God-Class_ganttproject-1.11.1.csv',
-        'JFreeChart-1.0.19/God-Class-JFreeChart-1.0.19.csv',
-        'jVLT-1.3.2/God-Class_jvlt-1.3.2.csv',
-        '88_jopenchart/God-Class_JOpenChart-v0.94.csv',
-    ],
+
     'FEATURE_ENVY': [
-        'tabula-java/Feature-Envy-Tabula-v1.0.6.csv',
+        '10_water-simulator/Feature-Envy_10_water-simulator-v2.csv',
+        '61_noen/Feature-Envy_Noen-v0.1.csv',
+        '88_jopenchart/Feature-Envy_JOpenChart-v0.94.csv',
+        '104_vuze/Feature-Envy_104_vuze.csv',
+        '105_freemind/Feature-Envy2_FreeMind-0.9.0.csv',
+        '107_weka/Feature-Envy2-Weka3.8.csv',
+        'commons-codec/Feature-Envy_Commons-codec-v1.15.csv',
+        'ganttproject_1_11_1_original/Feature-Envy2_ganttproject-1.11.1.csv',
+        'jfreechart-master/Feature-Envy2-JFreeChart-1.0.19.csv',
         'JSON20201115/Feature-Envy2_JASON-20201115.csv',
-        'FreeMind-0.9.0/Feature-Envy2_FreeMind-0.9.0.csv',
-        'Weka-3.8/Feature-Envy2-Weka3.8.csv',
-        'GanttProject-1.11.1/Feature-Envy2_ganttproject-1.11.1.csv',
-        'JFreeChart-1.0.19/Feature-Envy2-JFreeChart-1.0.19.csv',
         'jVLT-1.3.2/Feature-Envy_jvlt-1.3.2.csv',
-        '88_jopenchart/Feature_Envy_JOpenChart-v0.94.csv',
+        'tabula-java/Feature-Envy-Tabula-v1.0.6.csv',
+    ],
+
+    'GOD_CLASS': [
+        '10_water-simulator/God-Class_10_water-simulator-v2.csv',
+        '61_noen/God-Class_Noen-v0.1.csv',
+        '88_jopenchart/God-Class_JOpenChart-v0.94.csv',
+        '104_vuze/God-Class_104_vuze.csv',
+        '105_freemind/God-Class_FreeMind-0.9.0.csv',
+        '107_weka/God-Class-Weka3.8.csv',
+        'commons-codec/God-Class_Commons-codec-v1.15.csv',
+        'ganttproject_1_11_1_original/God-Class_ganttproject-1.11.1.csv',
+        'jfreechart-master/God-Class-JFreeChart-1.0.19.csv',
+        'JSON20201115/God-Class_JASON-20201115.csv',
+        'jVLT-1.3.2/God-Class_jvlt-1.3.2.csv',
+        'tabula-java/God-Class-Tabula-v1.0.6.csv',
     ],
 }
 
@@ -95,142 +128,201 @@ LONG_METHOD_PATH = os.path.join(CSV_ROOT_DIR, BENCHMARKS['LONG_METHOD'][BENCHMAR
 
 PROJECT_NAME = os.path.basename(PROJECT_PATH)
 
-# Initial value for qmood metrics and objectives to be used in normalization process
+# Initial value of QMOOD design metrics, testability and modularity used in objective-normalization process
 INITIAL_METRICS = {
-    "tabula-java": {'ANA': 0.631578947368421,
-                    'CAMC': 0.7792105263157896,
-                    'CIS': 4.092105263157895,
-                    'DAM': 0.38070175438596493,
-                    'DCC': 1.7236842105263157,
-                    'DSC': 67,
-                    'MFA': 0.0877063982997922,
-                    'MOA': 2.0657894736842106,
-                    'NOH': 1,
-                    'NOM': 5.184210526315789,
-                    'NOP': 3.210526315789474,
-                    'MODULE': 0.11403752430494184,
-                    #'TEST': 0.4287  # Obtaind by testability_prediction2
-                    'TEST': 0.3565407040728677  # Obtained by testability_prediction3
-                    },
-    "88_jopenchart": {
-        "DSC": 46,
-        "NOH": 4,
-        "ANA": 0.6923076923076923,
-        "MOA": 0.9230769230769231,
-        "DAM": 0.5291073124406457,
-        "CAMC": 0.5776923076923077,
-        "CIS": 8.025641025641026,
-        "NOM": 8.615384615384615,
-        "DCC": 2.358974358974359,
-        "MFA": 0.2705849314505054,
-        "NOP": 8.076923076923077,
-        "TEST": 0.4748841147083056,
-        "MODULE": 1,
+    "10_water-simulator": {  # 0
+        'ANA': 0.705357143,
+        'CAMC': 0.711339286,
+        'CIS': 2.571428571,
+        'DAM': 0.6224462,
+        'DCC': 3.625,
+        'DSC': 113.0,
+        'MFA': 0.033040629,
+        'MOA': 1.223214286,
+        'NOH': 1.0,
+        'NOM': 3.616071429,
+        'NOP': 3.026785714,
+        'MODULE': 0.22967147316783118,
+        'TEST2': 0.4878024731806904,  # Obtained by testability_prediction2
+        'TEST': 0.3702604117198275  # Obtained by testability_prediction3
     },
-    "JSON20201115": {
-        "DSC": 26,
-        "NOH": 2,
-        "ANA": 0.5,
-        "MOA": 1.2,
-        "DAM": 0.3055555555555556,
-        "CAMC": 0.8486666666666667,
-        "CIS": 9.5,
-        "NOM": 10.9,
-        "DCC": 2.8666666666666667,
-        "MFA": 0.099846547314578,
-        "NOP": 6.566666666666666,
-        "TEST": 0.4748841147083056,
-        "MODULE": 1,
+    "61_noen": {  # 1
+        'ANA': 0.498777506,
+        'CAMC': 0.688801956,
+        'CIS': 4.471882641,
+        'DAM': 0.757864181,
+        'DCC': 1.696821516,
+        'DSC': 428.0,
+        'MFA': 0.12371425,
+        'MOA': 1.701711491,
+        'NOH': 15.0,
+        'NOM': 4.951100244,
+        'NOP': 4.523227384,
+        'MODULE': 0.3743241335376458,
+        'TEST2': 0.5581233625170191,  # Obtained by testability_prediction2
+        'TEST': 0.5295360105264939  # Obtained by testability_prediction3
     },
-    "104_vuze": {
-        "DSC": 8040,
-        "NOH": 157,
-        "ANA": 0.6034383954154727,
-        "MOA": 0.9767908309455587,
-        "DAM": 0.2741500639139978,
-        "CAMC": 0.8217435530085977,
-        "CIS": 4.06676217765043,
-        "NOM": 5.139828080229226,
-        "DCC": 1.5714899713467048,
-        "MFA": 0.2091017384211249,
-        "NOP": 4.3729226361031515,
-        "TEST": 0.4748841147083056,
-        "MODULE": 0.20544581966389708,
+    "88_jopenchart": {  # 2
+        'ANA': 0.692307692,
+        'CAMC': 0.577692308,
+        'CIS': 8.025641026,
+        'DAM': 0.529107312,
+        'DCC': 2.358974359,
+        'DSC': 46.0,
+        'MFA': 0.270584931,
+        'MOA': 0.923076923,
+        'NOH': 4.0,
+        'NOM': 8.615384615,
+        'NOP': 8.076923077,
+        'MODULE': 0.20512820512820512,
+        'TEST2': 0.5516735761315649,  # Obtained by testability_prediction2
+        'TEST': 0.4817706859946628  # Obtained by testability_prediction3
     },
-    "105_freemind": {
-        "DSC": 1074,
-        "NOH": 23,
-        "ANA": 1.0050813008130082,
-        "MOA": 1.6747967479674797,
-        "DAM": 0.4325461206997841,
-        "CAMC": 0.7525609756097558,
-        "CIS": 4.728658536585366,
-        "NOM": 6.087398373983739,
-        "DCC": 2.9430894308943087,
-        "MFA": 0.1718928794350772,
-        "NOP": 5.1138211382113825,
-        "TEST": 0.3752525309440714,
-        "MODULE": 0.28767512755806884,
+    "104_vuze": {  # 3 Not ready
+        'ANA': 0.647412982,
+        'CAMC': 0.803941675,
+        'CIS': 4.467732832,
+        'DAM': 0.305707086,
+        'DCC': 1.729444967,
+        'DSC': 6221.0,
+        'MFA': 0.206624442,
+        'MOA': 1.029162747,
+        'NOH': 130.0,
+        'NOM': 5.634995296,
+        'NOP': 4.751834431,
+        "MODULE": 0,
+        "TEST2": 0,  # Obtained by testability_prediction2
+        'TEST': 0  # Obtained by testability_prediction3
     },
-    "107_weka": {
-        "DSC": 2338,
-        "NOH": 81,
-        "ANA": 0.9378711740520785,
-        "MOA": 2.3129282777523983,
-        "DAM": 0.4333987889293128,
-        "CAMC": 0.7152581087254457,
-        "CIS": 6.7907720420283235,
-        "NOM": 8.088624942896299,
-        "DCC": 2.385107354956601,
-        "MFA": 0.17212621102262438,
-        "NOP": 7.14846962083143,
-        "TEST": 0.4865105816109613,
-        "MODULE": 0.32787710443044066,
+    "105_freemind": {  # 4
+        'ANA': 0.927272727,
+        'CAMC': 0.738527273,
+        'CIS': 5.163636364,
+        'DAM': 0.46497322,
+        'DCC': 3.154545455,
+        'DSC': 626.0,
+        'MFA': 0.162760581,
+        'MOA': 1.590909091,
+        'NOH': 17.0,
+        'NOM': 6.474545455,
+        'NOP': 5.416363636,
+        'MODULE': 0.25275845632370597,
+        'TEST2': 0.4237480455902344,  # Obtained by testability_prediction2
+        'TEST': 0.3605321041480411,  # Obtained by testability_prediction3
     },
-    "ganttproject_1_11_1_original": {
-        "DSC": 555,
-        "NOH": 33,
-        "ANA": 0.6971544715447154,
-        "MOA": 1.508130081300813,
-        "DAM": 0.4831759334621796,
-        "CAMC": 0.736524390243902,
-        "CIS": 3.8536585365853657,
-        "NOM": 4.953252032520325,
-        "DCC": 2.680894308943089,
-        "MFA": 0.14711823738179727,
-        "NOP": 4.1971544715447155,
-        "TEST": 0.5029896831173097,
-        "MODULE": 0.34926607015973216,
+    "107_weka": {  # 5
+        'ANA': 0.894190871,
+        'CAMC': 0.561151452,
+        'CIS': 10.743775934,
+        'DAM': 0.636623387,
+        'DCC': 2.358921162,
+        'DSC': 1037.0,
+        'MFA': 0.346718339,
+        'MOA': 3.442946058,
+        'NOH': 75.0,
+        'NOM': 12.752074689,
+        'NOP': 11.32780083,
+        'MODULE': 0.20492201195677706,
+        'TEST2': 0,  # Obtained by testability_prediction2
+        'TEST': 0.37257823716205646,  # Obtained by testability_prediction3
     },
-    "jfreechart-master": {
-        "DSC": 674,
-        "NOH": 36,
-        "ANA": 1.0040160642570282,
-        "MOA": 4.546184738955823,
-        "DAM": 0.5328309085326908,
-        "CAMC": 0.7099598393574299,
-        "CIS": 10.238286479250334,
-        "NOM": 11.46318607764391,
-        "DCC": 3.317269076305221,
-        "MFA": 0.2586029996567078,
-        "NOP": 10.210174029451139,
-        "TEST": 0.4820787549441007,
-        "MODULE": 0.23642449145114441,
+    "commons-codec": {  # 6
+        'ANA': 0.32,
+        'CAMC': 0.7213,
+        'CIS': 6.92,
+        'DAM': 0.597956349,
+        'DCC': 1.85,
+        'DSC': 108.0,
+        'MFA': 0.125396441,
+        'MOA': 0.54,
+        'NOH': 7.0,
+        'NOM': 9.15,
+        'NOP': 4.18,
+        'MODULE': 0.4198853053622194,
+        'TEST2': 0,  # Obtained by testability_prediction2
+        'TEST': 0.4653682475404442  # Obtained by testability_prediction3
     },
-    "jvlt-1.3.2": {
-        "DSC": 420,
-        "NOH": 17,
-        "ANA": 0.8073170731707318,
-        "MOA": 2.4,
-        "DAM": 0.6101752259758623,
-        "CAMC": 0.7478780487804875,
-        "CIS": 4.021951219512195,
-        "NOM": 4.8951219512195125,
-        "DCC": 2.5585365853658537,
-        "MFA": 0.22910838294407537,
-        "NOP": 4.131707317073171,
-        "TEST": 0.42116082618406314,
-        "MODULE": 0.20757585289828134,
+    "ganttproject_1_11_1_original": {  # 7
+        'ANA': 0.574285714,
+        'CAMC': 0.730228571,
+        'CIS': 4.157142857,
+        'DAM': 0.5197205,
+        'DCC': 2.74,
+        'DSC': 407.0,
+        'MFA': 0.156224894,
+        'MOA': 1.577142857,
+        'NOH': 28.0,
+        'NOM': 5.491428571,
+        'NOP': 4.645714286,
+        "MODULE": 0.33473127930074126,
+        "TEST2": 0,  # Obtained by testability_prediction2
+        "TEST": 0.4426126215132276,  # Obtained by testability_prediction3
+
+    },
+    "jfreechart-master": {  # 8
+        'ANA': 1.036723164,
+        'CAMC': 0.709943503,
+        'CIS': 9.950564972,
+        'DAM': 0.544492767,
+        'DCC': 3.433615819,
+        'DSC': 633.0,
+        'MFA': 0.270958496,
+        'MOA': 2.892655367,
+        'NOH': 27.0,
+        'NOM': 11.211864407,
+        'NOP': 9.991525424,
+        "MODULE": 0.2544013611666677,
+        "TEST2": 0,  # Obtained by testability_prediction2
+        "TEST": 0.470488065430968,  # Obtained by testability_prediction3
+
+    },
+    "JSON20201115": {  # 9
+        'ANA': 0.5,
+        'CAMC': 0.848666667,
+        'CIS': 9.5,
+        'DAM': 0.305555556,
+        'DCC': 2.866666667,
+        'DSC': 26.0,
+        'MFA': 0.099846547,
+        'MOA': 1.2,
+        'NOH': 2.0,
+        'NOM': 10.9,
+        'NOP': 7.266666667,
+        "MODULE": 0.0,  # JASON project have no packages
+        "TEST2": 0,  # Obtained by testability_prediction2
+        "TEST": 0.4713493233566381,  # Obtained by testability_prediction3
+    },
+    "jvlt-1.3.2": {  # 10
+        'ANA': 0.763513514,
+        'CAMC': 0.710135135,
+        'CIS': 4.966216216,
+        'DAM': 0.659427284,
+        'DCC': 2.148648649,
+        'DSC': 160.0,
+        'MFA': 0.297960026,
+        'MOA': 1.75,
+        'NOH': 11.0,
+        'NOM': 5.418918919,
+        'NOP': 4.797297297,
+        "MODULE": 0.3051931655584547,
+        "TEST2": 0,  # Obtained by testability_prediction2
+        "TEST": 0.4787325664033668,  # Obtained by testability_prediction3
+    },
+    "tabula-java": {  # 11
+        'ANA': 0.623376623,
+        'CAMC': 0.763636364,
+        'CIS': 4.064935065,
+        'DAM': 0.374758575,
+        'DCC': 1.727272727,
+        'DSC': 68.0,
+        'MFA': 0.086567354,
+        'MOA': 2.051948052,
+        'NOH': 1.0,
+        'NOM': 5.142857143,
+        'NOP': 3.194805195,
+        'MODULE': 0.08199833867331198,
+        'TEST2': 0,  # Obtained by testability_prediction2
+        'TEST': 0.39981560024235097  # Obtained by testability_prediction3
     },
 }
 
@@ -238,6 +330,7 @@ CURRENT_METRICS = INITIAL_METRICS.get(PROJECT_NAME)
 date_time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 LOG_FILE = f'../sbse/logs/{PROJECT_NAME}-{date_time}.log'
 FORMATTER = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+
 
 # logging.basicConfig(
 #     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -260,7 +353,7 @@ def get_file_handler():
 
 def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG) # better to have too much log than not enough
+    logger.setLevel(logging.DEBUG)  # better to have too much log than not enough
     logger.addHandler(get_console_handler())
     logger.addHandler(get_file_handler())
     # with this pattern, it's rarely necessary to propagate the error up to parent
@@ -269,6 +362,7 @@ def get_logger(logger_name):
 
 
 logger = get_logger(__name__)
+
 
 # print(logger.handlers)
 
@@ -285,4 +379,3 @@ def log_project_info():
     logger.info(f"Crossover probability: {CROSSOVER_PROBABILITY}")
     logger.info(f"Mutation probability: {MUTATION_PROBABILITY}")
     logger.info("============ End of Configuration ============")
-
