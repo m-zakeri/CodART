7775e99728e6c1e6f9a7b6c10b3dffb9cb447a2d
m-zakeri <m-zakeri@live.com>
2022-04-05 13:29:18 +0430

    Add long method smell reader class

--- codart/smells/long_method.py
+++ codart/smells/long_method.py
@@ -2,14 +2,14 @@ import csv
 
 
 class CandidateReader:
-    def __init__(self, addr:str):
-        csv_file =  open(addr, newline='')
+    def __init__(self, path: str):
+        csv_file = open(path, newline='')
         data = csv.reader(csv_file)
         self.data = []
         for row in data:
             self.data.append(row)
 
-    def get_conf(self,index:int):
+    def get_conf(self, index: int):
         _conf = {
             'target_file': self.data[index][0],
             'lines': [int(x) for x in str(self.data[index][2]).split('::')],
