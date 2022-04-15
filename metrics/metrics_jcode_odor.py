02f0b0a2c56dac6678f77155bdf44053df62a754
m-zakeri <m-zakeri@live.com>
2022-04-15 23:05:40 +0430

    Apply PEP 8

--- metrics/metrics_jcode_odor.py
+++ metrics/metrics_jcode_odor.py
@@ -34,19 +34,19 @@ class JCodeOdorMetric:
 
     # Must be completed
     def is_abstract_annotation_interface(self, funcname):
-        if (str(funcname).startswith(("get", "set", "Set", "Get"))):
+        if str(funcname).startswith(("get", "set", "Set", "Get")):
             return True
         else:
             return False
 
     def MAXNESTING(self, funcname):
-        if (self.is_abstract_annotation_interface(funcname)):
+        if self.is_abstract_annotation_interface(funcname):
             return 0
         else:
             return funcname.metric(["MaxNesting"])["MaxNesting"]
 
     def CYCLO(self, funcname):
-        if (self.is_abstract_annotation_interface(funcname)):
+        if self.is_abstract_annotation_interface(funcname):
             return 0
         else:
             return funcname.metric(["Cyclomatic"])["Cyclomatic"]
@@ -109,7 +109,7 @@ class JCodeOdorMetric:
         LOC = class_name.metric(["CountLine"])["CountLine"]
         LOCAMM = 0
         for mth in class_name.ents('class_name', 'method'):
-            if (str(mth).startswith(("get", "set", "Set", "Get"))):
+            if str(mth).startswith(("get", "set", "Set", "Get")):
                 LOCAMM += mth.metric(["CountLine"])["CountLine"]
         return LOC - LOCAMM
 
@@ -133,15 +133,15 @@ class JCodeOdorMetric:
         if total == 0:
             return 0
         else:
-            return (count_functionl / total)
+            return count_functionl / total
 
     def WMCNAMM(self, class_name):
-        sum = 0
+        sum_ = 0
         for mth in class_name.ents('class_name', 'method'):
             if not (self.is_accesor_or_mutator(mth)):
-                sum += int(
+                sum_ += int(
                     0 if mth.metric(["Cyclomatic"])["Cyclomatic"] is None else mth.metric(["Cyclomatic"])["Cyclomatic"])
-        return sum
+        return sum_
 
     def TCC(self, class_name):
         NDC = 0
@@ -154,37 +154,36 @@ class JCodeOdorMetric:
 
         for row in range(0, len(method_list_visible)):
             for col in range(0, len(method_list_visible)):
-                if (row > col):
-                    if (self.connectivity(method_list_visible[row], method_list_visible[col])):
+                if row > col:
+                    if self.connectivity(method_list_visible[row], method_list_visible[col]):
                         NDC += 1
 
         N = len(method_list_visible)
         NP = N * (N - 1) / 2
-        if (NP != 0):
+        if NP != 0:
             return NDC / NP
         else:
             return 0
 
     def connectivity(self, row, col):
-        if (self.connectivity_directly(row, col) or self.connectivity_indirectly(row, col)):
+        if self.connectivity_directly(row, col) or self.connectivity_indirectly(row, col):
             return True
         else:
             return False
 
     def connectivity_indirectly(self, row, col):
-        # print(" connectivity_indirectly :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ")
         listrow = set()
         listcol = set()
         for callrow in row.refs("call"):
-            if (str(callrow.ent().name()).startswith(("get", "Get"))):
+            if str(callrow.ent().name()).startswith(("get", "Get")):
                 listrow.add(callrow.ent().longname())
 
         for callcol in col.refs("call"):
-            if (str(callcol.ent().name()).startswith(("get", "Get"))):
+            if str(callcol.ent().name()).startswith(("get", "Get")):
                 listcol.add(callcol.ent().longname())
 
         intersect = [value for value in listrow if value in listcol]
-        if (len(intersect) > 0):
+        if len(intersect) > 0:
             return True
         else:
             return False
@@ -199,7 +198,7 @@ class JCodeOdorMetric:
             listcol.add(callcol.ent().longname())
 
         intersect = [value for value in listrow if value in listcol]
-        if (len(intersect) > 0):
+        if len(intersect) > 0:
             return True
         else:
             return False
@@ -208,19 +207,18 @@ class JCodeOdorMetric:
         flag = False
         par = funcname.ents("funcname", "Parameter")
         for p in par:
-            if (str(p.type()) == "EventArgs"):
+            if str(p.type()) == "EventArgs":
                 flag = True
                 break
-        if not (str(funcname.kind()) == "Public Constructor") or not (flag) or not (
-                str(funcname.kind()) == "Private Method"):
+        if not str(funcname.kind()) == "Public Constructor" or not flag or not str(funcname.kind()) == "Private Method":
             return True
         else:
             return False
 
     def get_Namespace(self, entity):
-        while (str(entity.parent().kind()) != "Unresolved Namespace" or str(entity.parent().kind()) != "Namespace"):
+        while str(entity.parent().kind()) != "Unresolved Namespace" or str(entity.parent().kind()) != "Namespace":
             entity = entity.parent()
-            if (str(entity.parent().kind()) == "Unresolved Namespace" or str(entity.parent().kind()) == "Namespace"):
+            if str(entity.parent().kind()) == "Unresolved Namespace" or str(entity.parent().kind()) == "Namespace":
                 break
         return entity.parent()
 
@@ -239,18 +237,12 @@ class JCodeOdorMetric:
         if cint == 0:
             return 0
         else:
-            return (method_name.metric(["CountOutPut"]) / cint)
+            return method_name.metric(["CountOutPut"]) / cint
 
-    def NOPK(self, db):
+    def NOCS(self, input_):
         count = 0
-        for package in db.ents("Package"):
-            count += 1
-        return count
-
-    def NOCS(self, input):
-        count = 0
-        classes = input.ents('Define', 'class')
-        if (len(classes) > 0):
+        classes = input_.ents('Define', 'class')
+        if len(classes) > 0:
             count += len(classes)
             for cls in classes:
                 count += self.NOCS(cls)
@@ -258,16 +250,16 @@ class JCodeOdorMetric:
             count += 0
         return count
 
-    def NOCS_Package(self, input):
-        return input.metric(["CountDeclClass"])["CountDeclClass"]
+    def NOCS_Package(self, input_):
+        return input_.metric(["CountDeclClass"])["CountDeclClass"]
 
-    def NOCS_Project(self, input):
-        return input.metric(["CountDeclClass"])["CountDeclClass"]
+    def NOCS_Project(self, input_):
+        return input_.metric(["CountDeclClass"])["CountDeclClass"]
 
     def NOM(self, class_name):
         um = 0
         for mth in class_name.ents('Define', 'method'):
-            if (not mth.refs('Override')):
+            if not mth.refs('Override'):
                 um += 1
         return um
 
@@ -287,7 +279,7 @@ class JCodeOdorMetric:
         if cint == 0:
             return 0
         else:
-            return (self.WMCNAMM(class_name) / cint)
+            return self.WMCNAMM(class_name) / cint
 
     def NOP(self, method_name):
         count = 0
@@ -311,17 +303,16 @@ class JCodeOdorMetric:
         return CountDeclMethodAll - CountDeclMethod
 
     def LCOM5(self, class_name):
-        result = 0
         NOACC = 0
         NOM = self.NOM(class_name)
         NOA = self.NOA(class_name)
         entities = class_name.ents('Define', 'Variable')
         for enty in entities:
             for ref in enty.refs('Useby'):
-                if ((ref.ent().kind() != "Constructor")):
+                if "Constructor" not in ref.ent().kind():
                     NOACC += 1
 
-        if (NOM > 1 and NOA > 0):
+        if NOM > 1 and NOA > 0:
             result = (NOM - (NOACC / NOA)) / (NOM - 1)
         else:
             result = 0
@@ -371,7 +362,7 @@ class JCodeOdorMetric:
     def NOII(self, class_name):
         count = 0
         for b in class_name.ents('Implement'):
-            if (b.kindname() == "Interface"):
+            if b.kindname() == "Interface":
                 count += 1
         return count
 
@@ -388,7 +379,7 @@ class JCodeOdorMetric:
         list1 = list()
         if not (self.is_abstract_annotation_interface(method_name)):
             for meth in method_name.refs('Use', 'Variable'):
-                if (meth.ent().parent().longname() != method_name.parent().longname()):
+                if meth.ent().parent().longname() != method_name.parent().longname():
                     list1.append(meth.ent().parent().parent())
         return len(set(list1))
 
@@ -409,16 +400,16 @@ class JCodeOdorMetric:
         if not (self.is_abstract_annotation_interface(method_name)):
             for meth in method_name.refs('Use', 'Variable'):
                 listtotal.append(meth)
-                if (meth.ent().parent().longname() == method_name.parent().longname()):
+                if meth.ent().parent().longname() == method_name.parent().longname():
                     listsameclass.append(meth)
             for meth in method_name.refs('call', 'method'):
                 if meth.ent().kindname() != "Public Constructor":
-                    if (self.is_accesor_or_mutator(meth.ent())):
+                    if self.is_accesor_or_mutator(meth.ent()):
                         listtotal.append(meth)
-                    if (self.is_accesor_or_mutator(
-                            meth.ent()) and meth.ent().parent().longname() == method_name.parent().longname()):
+                    if self.is_accesor_or_mutator(
+                            meth.ent()) and meth.ent().parent().longname() == method_name.parent().longname():
                         listsameclass.append(meth)
-        if (len(set(listtotal)) != 0):
+        if len(set(listtotal)) != 0:
             result = len(set(listsameclass)) / len(set(listtotal))
         return result
 
@@ -440,7 +431,7 @@ class JCodeOdorMetric:
                 count += 1
             for meth in method_name.refs('call', 'method'):
                 if meth.ent().kindname() != "Public Constructor":
-                    if (self.is_accesor_or_mutator(meth.ent())):
+                    if self.is_accesor_or_mutator(meth.ent()):
                         for mth in meth.ent().refs('Use', 'Variable'):
                             count += 1
         return count
@@ -453,20 +444,17 @@ class JCodeOdorMetric:
         return access_field_list
 
     def give_Methods_that_the_measured_method_calls(self, funcname):
-        # create a list and return it:Includes all Methods entity(also cunstructor method ) that the measured method calls
+        """create a list and return it:Includes all Methods entity(also cunstructor method )
+        that the measured method calls
+        """
         call_methods_list = set()
         for fi in funcname.refs("call"):
             # if namespace == method namespace
-            if (fi.ent().parent().parent() == funcname.parent().parent()):
+            if fi.ent().parent().parent() == funcname.parent().parent():
                 # print(fi,"->",fi.ent().parent().parent())
                 # print("_")
                 call_methods_list.add(fi.ent())
         return call_methods_list
-
-    def result(self, db):
-        self.get_metrics(db)
-        return [self.class_metrics, self.method_metrics]
-        # return a list consist of classes and methods and thier metrics value
 
     def ATFD(self, class_entity):
         foreign_data = class_entity.ents('Useby', 'Java Variable')
