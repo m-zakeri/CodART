9fc29f9283b243f033fb9a91d9e9c509c33bf586
m-zakeri <m-zakeri@live.com>
2022-04-18 13:19:24 +0430

    Fix bugs

--- refactorings/extract_class.py
+++ refactorings/extract_class.py
@@ -259,13 +259,15 @@ class ExtractClassRefactoringListener(JavaParserLabeledListener):
             field_type = ctx.typeType().getText()
 
             if len(field_names) == 1:
+                # Todo: Required better handling
                 st = f"public {field_type} {field_names[0]};{self.NEW_LINE}"
-                if '=new' in st and '<>' in st:
+                if '=new' in st and '()' in st:
                     st = st.replace('new', 'new ')
                 self.code += st
             else:
+                # Todo: Required better handling
                 st = f"public {field_type} {self.detected_field};{self.NEW_LINE}"
-                if '=new' in st and '<>' in st:
+                if '=new' in st and '()' in st:
                     st = st.replace('new', 'new ')
                 self.code += st
             # delete field from source class
