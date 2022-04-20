3a298e7a19e18621ffb37ac497eb2b9ea116ebfd
m-zakeri <m-zakeri@live.com>
2022-04-21 00:17:09 +0430

    Add executing a refactoring population saved in json file.

--- sbse/sequence_application.py
+++ sbse/sequence_application.py
@@ -17,22 +17,20 @@ import pandas as pd
 import sbse.config as config
 from codart.utility.directory_utils import reset_project, update_understand_database
 from sbse.initialize import REFACTORING_MAIN_MAP
+from sbse.search_based_refactoring2 import log_project_info
 
 from metrics import qmood
 from metrics.modularity import main as modularity_main
 from metrics.testability_prediction2 import main as testability_main
 
-# project_dir_old = 'C:\\Users\\Administrator\\Downloads\\udbs'
-# udb_path_old = 'C:/Users/Administrator/Downloads/udbs\\10_water-simulator.udb'
-file_path_base_dir_old = 'C:\\Users\\Administrator\\Downloads\\prj_src'  # System server2 path (to be replaced)
-
-
-# file_path_base_dir_old = 'C:\\Users\\Administrator\\Downloads\\IdeaProjects2_Cleaned'  # For JOpenChart project only
 
 class RefactoringSequenceEvaluation:
     def __init__(self, log_directory):
         super(RefactoringSequenceEvaluation, self).__init__()
         self.log_directory = log_directory
+        # self.project_dir_old = 'C:\\Users\\Administrator\\Downloads\\udbs'
+        # self.udb_path_old = 'C:/Users/Administrator/Downloads/udbs\\10_water-simulator.udb'
+        self.file_path_base_dir_old = 'C:\\Users\\Administrator\\Downloads\\prj_src'  # Server2 path (to be replaced)
 
     def evaluate_sequences(self):
         initial_values_path = glob.glob(f'{self.log_directory}quality_attrs_initial_values.csv')[0]
@@ -52,6 +50,7 @@ class RefactoringSequenceEvaluation:
             col_names = ['generation', 'testability']
         else:
             col_names = None
+            return
 
         df_res = pd.read_csv(
             res_path,
@@ -82,47 +81,90 @@ class RefactoringSequenceEvaluation:
         print(df_final_result)
         df_final_result.to_csv(f'{config.PROJECT_LOG_DIR}evaluation_results.csv', index=False)
 
+    def execute_from_json_log(self, input_file_path=None):
+        if input_file_path is None:
+            input_file_path = glob.glob(f'{self.log_directory}best_refactoring_sequences*.json')[0]
 
-def execute_from_log(input_file_path):
-    """
-    input_file_path: The path of input data from log.
+        population = []
+        with open(input_file_path, 'r', encoding='utf-8') as fp:
+            population_trimmed = json.load(fp)
 
-    Example: Take a look at ./input.txt
-    """
+        for chromosome in population_trimmed:
+            chromosome_new = []
+            for gene_ in chromosome:
+                refactoring_params = gene_[1]
+                if refactoring_params.get('udb_path'):
+                    refactoring_params['udb_path'] = config.UDB_PATH
+                if refactoring_params.get('project_dir'):
+                    refactoring_params['project_dir'] = config.PROJECT_PATH
+                if refactoring_params.get('file_path'):
+                    refactoring_params['file_path'] = refactoring_params['file_path'].replace(
+                        self.file_path_base_dir_old,
+                        config.PROJECT_ROOT_DIR
+                    )
+                chromosome_new.append((REFACTORING_MAIN_MAP[gene_[0]], refactoring_params, gene_[0]))
+            population.append(chromosome_new)
 
-    with open(input_file_path, 'r') as f:
-        data = f.read().split('\n')
+        print(population)
+        # quit()
+        for refactoring_sequence in population:
+            reset_project()
+            # Apply sequence X to system S
+            for refactoring_operation in refactoring_sequence:
+                res = refactoring_operation[0](**refactoring_operation[1])
+                update_understand_database(config.UDB_PATH)
+                print(f"Executed {refactoring_operation[2]} with status {res}")
 
-    for row in data:
-        refactoring_name = re.search(', (.+?)(\w+)*\(', row).group()[1:-1].strip()
-        params = re.search('{(.+?)}', row).group().strip()
-        params = params.replace("'", '"')
-        params = params.replace("False", "false")
-        params = params.replace("True", "true")
-        params = json.loads(params)
+            # Compute quality metrics
+            log_project_info(
+                reset_=False,
+                quality_attributes_path=f'{config.PROJECT_LOG_DIR}best_refactoring_sequences_objectives_extended.csv',
+                generation=config.MAX_ITERATIONS
+            )
 
-        if params.get('udb_path'):
-            params['udb_path'] = config.UDB_PATH
-        if params.get('project_dir'):
-            params['project_dir'] = config.PROJECT_PATH
-        if params.get('file_path'):
-            params['file_path'] = params['file_path'].replace(file_path_base_dir_old, config.PROJECT_ROOT_DIR)
-        print(refactoring_name, params)
+        reset_project()
 
-        main_function = REFACTORING_MAIN_MAP[refactoring_name](**params)
-        print(f"Executed {refactoring_name} with status {main_function}")
-        update_understand_database(config.UDB_PATH)
-        print('-' * 100)
+    def execute_from_txt_log(self, input_file_path):
+        """
+        input_file_path: The path of input data from log.
+
+        Example: Take a look at ./input.txt
+        """
+
+        with open(input_file_path, 'r') as f:
+            data = f.read().split('\n')
+
+        for row in data:
+            refactoring_name = re.search(', (.+?)(\w+)*\(', row).group()[1:-1].strip()
+            params = re.search('{(.+?)}', row).group().strip()
+            params = params.replace("'", '"')
+            params = params.replace("False", "false")
+            params = params.replace("True", "true")
+            params = json.loads(params)
+
+            if params.get('udb_path'):
+                params['udb_path'] = config.UDB_PATH
+            if params.get('project_dir'):
+                params['project_dir'] = config.PROJECT_PATH
+            if params.get('file_path'):
+                params['file_path'] = params['file_path'].replace(self.file_path_base_dir_old, config.PROJECT_ROOT_DIR)
+            print(refactoring_name, params)
+
+            res = REFACTORING_MAIN_MAP[refactoring_name](**params)
+            print(f"Executed {refactoring_name} with status {res}")
+            update_understand_database(config.UDB_PATH)
+            print('-' * 100)
 
 
 def execute_refactoring_sequence():
     directory_name_ = os.path.dirname(__file__)
     refactoring_sequence_input_file = os.path.join(directory_name_, r'utils/input.txt')
     # reset_project()
-    # execute_from_log(input_file_path=refactoring_sequence_input_file)
+    # execute_from_txt_log(input_file_path=refactoring_sequence_input_file)
 
 
 if __name__ == '__main__':
     # execute_refactoring_sequence()
     eval_ = RefactoringSequenceEvaluation(log_directory=config.PROJECT_LOG_DIR)
     eval_.evaluate_sequences()
+    eval_.execute_from_json_log()
