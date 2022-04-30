7583e5030456b130fc9ba33745a8272a10329740
m-zakeri <m-zakeri@live.com>
2022-04-30 13:44:25 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+"""
+CodART setup script
+
+"""
+
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -5,35 +10,52 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="codart",  # Replace with your own username
-    version="0.1.0dev",
+    version="0.1.1dev",
     author="Morteza Zakeri",
     author_email="m-zakeri@live.com",
-    description="Source code automated refactoring toolkit",
+    description="CodART: Free source code automated refactoring toolkit",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/m-zakeri/CodART",
+
     project_urls={
         "Bug Tracker": "https://github.com/m-zakeri/CodART/issues",
     },
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
     package_dir={
+        'codart': 'codart',
         'gen': 'gen',
         'metrics': 'metrics',
         'refactoring_design_patterns': 'refactoring_design_patterns',
         'refactorings': 'refactorings',
         'sbse': 'sbse',
-        'visualization': 'visualization',
+
     },
-    packages=setuptools.find_packages(include=['refactorings', 'refactorings.*',
-												'refactoring_design_patterns', 'refactoring_design_patterns.*',
-                                               'gen', 'gen.*',
-                                               'metrics',
-                                               'sbse',
-                                               'visualization',
-                                               ]),
-    python_requires=">=3.5",
+
+    packages=setuptools.find_packages(
+        include=
+        [
+            'codart',
+            'codart.*'
+            'gen',
+            'gen.*',
+            'metrics',
+            'metrics.*',
+            'refactoring_design_patterns',
+            'refactoring_design_patterns.*',
+            'refactorings',
+            'refactorings.*',
+            'sbse',
+            'sbse.*',
+
+        ]
+    ),
+
+    python_requires=">=3.8",
 )
