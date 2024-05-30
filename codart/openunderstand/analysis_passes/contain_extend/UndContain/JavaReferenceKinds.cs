967c7e5326f866640ab2ba6fd9c989094e30480d
aminhzdev <amin.hasan.zarei@gmail.com>
2024-05-30 19:35:38 +0330

    remove extra files

--- codart/openunderstand/analysis_passes/contain_extend/UndContain/JavaReferenceKinds.cs
+++
@@ -1,9 +0,0 @@
-namespace UndContain;
-
-public static class JavaReferenceKinds
-{
-    public static readonly string Contain = "Java Contain";
-    public static readonly string ContainIn = "Java Containin";
-    public static readonly string Extend = "Java Extend Couple";
-    public static readonly string ExtendBy = "Java Extend Coupleby";
-}
